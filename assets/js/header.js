5d580e060eca67d3986d64ade2fd94d5a9f74590
story <70672236+quicklooked@users.noreply.github.com>
2020-09-16 19:13:15 +0700

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,5 +2,5 @@ const searchIcon = document.getElementById('search-icon');
 const searchInput = document.getElementById('search-input');
 searchIcon.addEventListener('click', () => {
   searchInput.style.width = '200px';
-  searchInput.focus();       
+  searchInput.focus();
 });
