1bcc182c2a3fb38d85ad06b17e546a106d52da3f
story <70672236+quicklooked@users.noreply.github.com>
2020-09-16 17:57:25 +0700

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,33 +1,25 @@
 const carouselForward = document.querySelectorAll('.carousel-forward');
 const carouselReverse = document.querySelectorAll('.carousel-reverse');
 let resizing = false;
-
 const updateNavWidth = () => {
     const carouselInner = document.querySelector('.carousel-navigation-inner');
     let carouselInnerWidth = 0;
-
     for (let i = 0; i < carouselInner.children.length; i++) {
         carouselInnerWidth += carouselInner.children[i].offsetWidth;
         carouselInnerWidth += parseFloat(window.getComputedStyle(carouselInner.children[i]).marginLeft);
         carouselInnerWidth += parseFloat(window.getComputedStyle(carouselInner.children[i]).marginRight);
     }
-
     carouselInner.style.width = `${carouselInnerWidth}px`;
     carouselInner.style.transform = "translateX(0)";
-
     // carousel forward button
     carouselInner.parentElement.nextElementSibling.children[0].disabled = false;
-
     // carousel reverse button
     carouselInner.parentElement.previousElementSibling.children[0].disabled = true;
 }
-
 const updateNavControls = () => {
     const carouselEL = document.querySelector('.carousel-navigation');
     const carouselInner = document.querySelector('.carousel-navigation-inner');
-
     const isCarouselSameWidth = Math.ceil(carouselEL.getBoundingClientRect().width) >= carouselInner.style.width.slice(0, -2);
-
     if (isCarouselSameWidth) {
         carouselForward[0].style.display = 'none';
         carouselReverse[0].style.display = 'none';
@@ -36,19 +28,15 @@ const updateNavControls = () => {
         carouselReverse[0].style.display = 'initial';
     }
 }
-
 carouselReverse.forEach((button) => {
     button.addEventListener('click', e => {
         if (e.target.nodeName === "BUTTON") {
             const carouselEl = e.target.parentElement.nextElementSibling;
             const carouselElInner = carouselEl.firstElementChild;
             const translateX = parseInt(window.getComputedStyle(carouselElInner).transform.slice(7, -1).split(',')[4]);
-
-            const distance = (e.target.getBoundingClientRect().right) - (carouselElInner.getBoundingClientRect().left);
-            
+            const distance = (e.target.getBoundingClientRect().right) - (carouselElInner.getBoundingClientRect().left);   
             if (distance - (carouselEl.getBoundingClientRect().width) < 0) {
                 carouselElInner.style.transform = "translateX(0)";
-                
                 // styling
                 e.target.disabled = true;
                 e.target.parentElement.nextElementSibling.nextElementSibling.children[0].disabled = false;
@@ -59,19 +47,15 @@ carouselReverse.forEach((button) => {
         }
     });
 });
-
 carouselForward.forEach((button) => {
     button.addEventListener('click', e => {
         if (e.target.nodeName === "BUTTON") {
             const carouselEl = e.target.parentElement.previousElementSibling;
             const carouselElInner = carouselEl.firstElementChild;
             const translateX = parseInt(window.getComputedStyle(carouselElInner).transform.slice(7, -1).split(',')[4]);        
-
             const distance = (e.target.getBoundingClientRect().left) - (carouselElInner.getBoundingClientRect().right);
-
             if (distance + (carouselEl.getBoundingClientRect().width) > 0) {
                 carouselElInner.style.transform = `translateX(-${carouselElInner.getBoundingClientRect().width - carouselEl.getBoundingClientRect().width}px)`;
-
                 // styling
                 e.target.disabled = true;
                 e.target.parentElement.previousElementSibling.previousElementSibling.children[0].disabled = false;
@@ -82,11 +66,9 @@ carouselForward.forEach((button) => {
         }
     });
 });
-
 window.addEventListener('resize', () => {
     resizing = true;
 });
-
 setInterval(() => {
     if (resizing) {
         resizing = false;
@@ -94,6 +76,5 @@ setInterval(() => {
         updateNavControls();
     }
 }, 250);
-
 updateNavWidth();
 updateNavControls();
