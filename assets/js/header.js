674eedef333dd9b1a8e1d9b8ba75e23464b6340e
story <70672236+quicklooked@users.noreply.github.com>
2020-09-18 12:43:51 +0700

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,109 +1,62 @@
-        z.eS = function(a, b, c) {
-                a = a || {};
-                var d = a.Lf
-                  , e = a.type;
-                b = z.v;
-                "insetArrows" == e ? (a = a || {},
-                a = a.Lf,
-                c = (0,
-                z.v)('x3cdiv classx3d"u-flexCenter u-minWidth0"x3ex3cdiv classx3d"u-flex0 u-paddingRight10 js-carouselNavigation"x3e' + z.x(z.Km({
-                        icon: (0,
-                        z.M)("" + z.x(z.am({
-                                Cm: !0
-                        }))),
-                        action: "scroll-carousel",
-                        actionValue: -1,
-                        ea: !0,
-                        l: "js-carouselReverse"
-                }, c)) + 'x3c/divx3ex3cdiv classx3d"u-flex1 u-overflowHidden"x3ex3cdiv classx3d"u-flexTop js-carouselInner"x3e' + (a ? z.x(a) : "") + 'x3c/divx3ex3c/divx3ex3cdiv classx3d"u-flex0 u-marginLeftAuto u-paddingLeft10 js-carouselNavigation"x3e' + z.x(z.Km({
-                        icon: (0,
-                        z.M)("" + z.x(z.cm({
-                                Pg: !0
-                        }))),
-                        action: "scroll-carousel",
-                        ea: !0,
-                        l: "js-carouselForward"
-                }, c)) + "x3c/divx3ex3c/divx3e")) : c = 'x3cdiv classx3d"u-relative' + ("arrowsLarge" == e ? " u-sm-paddingLeft50 u-sm-paddingRight50" : "") + '"x3ex3cdiv classx3d"u-overflowHidden"x3ex3cdiv classx3d"u-flexTop js-carouselInner"x3e' + (d ? z.x(d) : "") + 'x3c/divx3ex3c/divx3ex3cdiv classx3d"js-carouselNavigation"x3ex3c/divx3ex3c/divx3e';
-                return b(c)
-        }
-        ;
+function getX(item) {
+  var currTrans = item.css("transform");
 
+  if (currTrans == "none") {
+    return 0;
+  } else {
+    var transSplit = currTrans.split(/[()]/)[1];
+    var posx = transSplit.split(",")[4];
+    return Number(posx);
+  }
+}
 
-        oNa = function(a) {
-                var b = a.VF.length;
-                1 == b ? a.bb(".js-carouselNavigation").forEach(function(a) {
-                        return z.zs(a)
-                }) : a.lG != rNa && z.cw(a.query(".js-carouselNavigation"), VLa, {
-                        type: a.lG,
-                        SFa: b
-                })
-        }
-        ;
+function getOverflow() {
+  console.log($(".js-carouselInner").parent()[0]);
+  var overflow = $(".js-carouselInner").parent()[0].scrollWidth;
+  var width = $(".js-carouselInner").width();
+  var ow = overflow - width;
+  console.log(ow);
+  return ow;
+}
 
-        z.Je("r", "arrows", function(a, b, c) {
-                return (0,
-                z.v)('x3cdiv classx3d"u-flexColumn u-absolute u-top0 u-leftNegative25 u-xs-leftNegative20 u-bottom0"x3e' + z.x(z.Km({
-                        icon: (0,
-                        z.M)("" + z.x(z.am({
-                                Cm: !0
-                        }))),
-                        ae: !0,
-                        action: "scroll-carousel",
-                        actionValue: -1,
-                        ea: !0,
-                        l: "js-carouselReverse"
-                }, c)) + 'x3c/divx3ex3cdiv classx3d"u-flexColumn u-absolute u-top0 u-rightNegative20 u-xs-rightNegative10 u-bottom0"x3e' + z.x(z.Km({
-                        icon: (0,
-                        z.M)("" + z.x(z.cm({
-                                Pg: !0
-                        }))),
-                        ae: !0,
-                        action: "scroll-carousel",
-                        ea: !0,
-                        l: "js-carouselForward"
-                }, c)) + "x3c/divx3e")
-        });
+var ow = 0;
 
+$(window).on("resize", function() {
+  ow = getOverflow();
+  $(".js-carouselInner").css({ transform: "translateX(0)" });
+  $(".js-carouselReverse").prop("disabled", true);
+  $(".js-carouselForward").prop("disabled", false);
+});
 
-        z.Je("r", "arrowsLarge", function(a, b, c) {
-                return (0,
-                z.v)('x3cdiv classx3d"u-flexColumn u-absolute u-top0 u-leftNegative40 u-bottom0 u-sm-left0"x3e' + z.x(z.Km({
-                        icon: (0,
-                        z.M)("" + z.x(z.Gea())),
-                        action: "scroll-carousel",
-                        actionValue: -1,
-                        ea: !1,
-                        qd: !0,
-                        l: "js-carouselReverse"
-                }, c)) + 'x3c/divx3ex3cdiv classx3d"u-flexColumn u-absolute u-top0 u-rightNegative40 u-bottom0 u-sm-right0"x3e' + z.x(z.Km({
-                        icon: (0,
-                        z.M)("" + z.x(z.bm(null))),
-                        action: "scroll-carousel",
-                        ea: !1,
-                        qd: !0,
-                        l: "u-relative u-top1 js-carouselForward"
-                }, c)) + "x3c/divx3e")
-        });
+$(document).ready(function() {
+  ow = getOverflow();
 
+  $(".js-carouselReverse").click(function(e) {
+    var x = getX($(".js-carouselInner"));
+    var difference = 274;
+    var minus = x + difference;
 
-        LT = function(a) {
-                var b = a.VF.length;
-                if (1 != b)
-                        switch (a.lG) {
-                        case z.HT:
-                                a.bb(".js-carouselCrumb").forEach(function(b, c) {
-                                        z.uc(b, "u-textColorGreenNormal", c == a.Nv)
-                                });
-                                break;
-                        default:
-                                var c = a.query(".js-carouselReverse")
-                                  , d = a.query(".js-carouselForward");
-                                0 == a.Nv ? (c.setAttribute("disabled", "disabled"),
-                                1 < b && d.removeAttribute("disabled")) : 1 < b && a.Nv >= b - 1 ? (c.removeAttribute("disabled"),
-                                d.setAttribute("disabled", "disabled")) : (c.removeAttribute("disabled"),
-                                d.removeAttribute("disabled"))
-                        }
-        }
-        ;
+    if (x <= -difference) {
+      $(".js-carouselInner").css({ transform: "translateX(" + minus + "px)" });
+      $(".js-carouselForward").prop("disabled", false);
+    } else {
+      $(".js-carouselInner").css({ transform: "translateX(0)" });
+      $(this).prop("disabled", true);
+    }
+  });
 
+  $(".js-carouselForward").click(function(e) {
+    var x = getX($(".js-carouselInner"));
+    var difference = -274;
+    var plus = x + difference;
+    console.log(x);
 
+    if (x > -(ow + difference)) {
+      $(".js-carouselInner").css({ transform: "translateX(" + plus + "px)" });
+      $(".js-carouselReverse").prop("disabled", false);
+    } else {
+      $(".js-carouselInner").css({ transform: "translateX(" + -ow + "px)" });
+      $(this).prop("disabled", true);
+    }
+  });
+});
