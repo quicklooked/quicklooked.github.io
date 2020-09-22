acb905e893b71c2f622da46f89c5949ef00d947e
story <70672236+quicklooked@users.noreply.github.com>
2020-09-22 15:05:05 +0700

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
 
   $(".js-carouselReverse").click(function(e) {
     var x = getX($(".js-carouselInner"));
-    var difference = 100;
+    var difference = 189;
     var minus = x + difference;
 
     if (x <= -difference) {
@@ -47,7 +47,7 @@ $(document).ready(function() {
 
   $(".js-carouselForward").click(function(e) {
     var x = getX($(".js-carouselInner"));
-    var difference = -100;
+    var difference = -189;
     var plus = x + difference;
     console.log(x);
 
