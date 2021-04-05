64d14daf29687d2420a507044e2c304fedaecc04
story <70672236+quicklooked@users.noreply.github.com>
2021-04-05 13:39:11 +0700

    Create greet.js

---
+++ assets/js/greet.js
@@ -0,0 +1,7 @@
+var myDate = new Date();
+var hrs = myDate.getHours();
+var greet;
+if(hrs < 12) greet = 'Good morning';
+else if(hrs >= 12 && hrs <= 17) greet = 'Good afternoon';
+else if(hrs >= 17 && hrs <= 24) greet = 'Good evening';
+document.getElementById('lblGreetings').innerHTML = greet;
