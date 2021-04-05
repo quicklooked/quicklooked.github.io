625df96ae64be0a092bb00828254feb0c24a9387
story <70672236+quicklooked@users.noreply.github.com>
2021-04-05 17:42:03 +0700

    Update greet.js

--- assets/js/greet.js
+++ assets/js/greet.js
@@ -4,4 +4,4 @@ var greet;
 if(hrs < 12) greet = 'Good morning';
 else if(hrs >= 12 && hrs <= 17) greet = 'Good afternoon';
 else if(hrs >= 17 && hrs <= 24) greet = 'Good evening';
-document.getElementById('lblGreetings').innerHTML = '<b>' + greet + '</b>;
+document.getElementById('lblGreetings').innerHTML = '<b>' + greet + '</b>';
