f964caf48999829cf337f7b8ee346ddc7f0618c5
story <70672236+quicklooked@users.noreply.github.com>
2021-04-07 03:42:12 +0700

    Create setMaxheight.js

---
+++ assets/js/setMaxheight.js
@@ -0,0 +1,23 @@
+function setMaxheight(){
+    var affixEle = $("#scroll").first(),
+        winHeight = $(window).height();
+
+    // If there is some margins from top or bottom, remove it like:
+    // winheight -= 20;
+
+    affixEle.css({
+        'max-height' : winHeight*62/100 + "px"
+    });
+}
+
+$(document).ready(function(){
+  setMaxheight();
+});
+
+$(function(){
+    
+
+    $(window).resize(function(){
+        setMaxheight();
+    });
+});
