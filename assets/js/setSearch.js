b52f7dd3130210a5a8ccf38710d87cb54cfc656b
story <70672236+quicklooked@users.noreply.github.com>
2021-04-09 10:17:46 +0700

    Create setSearch.js

---
+++ assets/js/setSearch.js
@@ -0,0 +1,14 @@
+function setSearch() {
+	if(tabIndex = -1) {
+		document.getElementById("search").tabIndex = 0;
+		document.getElementById("search").ariaHidden = "false";
+		document.getElementById("search").classList.remove("jw", "jx", "jy", "ek", "by", "jz", "ka", "bz", "kb", "gz", "kc", "kd", "ke");
+		document.getElementById("search").classList.add("jw", "jx", "jy", "ek", "by", "me", "ka", "bz", "kb", "gz", "mf", "lh", "ke");
+	}
+	else {
+		document.getElementById("search").tabIndex = -1;
+		document.getElementById("search").ariaHidden = "true";
+		document.getElementById("search").classList.remove("jw", "jx", "jy", "ek", "by", "me", "ka", "bz", "kb", "gz", "mf", "lh", "ke");
+		document.getElementById("search").classList.add("jw", "jx", "jy", "ek", "by", "jz", "ka", "bz", "kb", "gz", "kc", "kd", "ke");
+	}
+}
