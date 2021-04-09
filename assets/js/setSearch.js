e5541708a68cfd7cd85739b7b5ceb877b60c4d94
story <70672236+quicklooked@users.noreply.github.com>
2021-04-09 17:11:14 +0700

    Update setSearch.js

--- assets/js/setSearch.js
+++ assets/js/setSearch.js
@@ -5,6 +5,7 @@ function setSearch() {
 		document.getElementById("search").ariaHidden = "false";
 		document.getElementById("search").classList.remove("jw", "jx", "jy", "ek", "by", "jz", "ka", "bz", "kb", "gz", "kc", "kd", "ke");
 		document.getElementById("search").classList.add("jw", "jx", "jy", "ek", "by", "me", "ka", "bz", "kb", "gz", "mf", "lh", "ke");
+		document.getElementById("search").focus();
 	} else {
 		document.getElementById("search").tabIndex = -1;
 		document.getElementById("search").ariaHidden = "true";
