cd61cd52b2c15fd176582c92f30fe64223d3cab0
story <70672236+quicklooked@users.noreply.github.com>
2021-04-11 16:11:45 +0700

    Update slideAvatar.js

--- assets/js/slideAvatar.js
+++ assets/js/slideAvatar.js
@@ -1,10 +1,19 @@
-function setSlide() {
-	var attr = document.getElementById("slide").attributes;
-	if(attr['aria-hidden'].value == "true") {
-		document.getElementById("slide").classList.remove("ko", "fu", "fq", "ms", "v", "ny", "fk", "mu", "nz");
-		document.getElementById("slide").classList.add("ko", "fu", "fi", "ms", "v", "ot", "fk", "mu", "nz");
-	} else {
-		document.getElementById("search").classList.remove("ko", "fu", "fi", "ms", "v", "ot", "fk", "mu", "nz");
-		document.getElementById("search").classList.add("ko", "fu", "fq", "ms", "v", "ny", "fk", "mu", "nz");
-	}
-}
+document.addEventListener("DOMContentLoaded", function() {
+    var status = "under300";
+    window.addEventListener("scroll", function() {
+        var x = pageYOffset;
+        if(x > 300) {
+            if(status == "under300") {
+                document.getElementById("slide").classList.remove("ko", "fu", "fq", "ms", "v", "ny", "fk", "mu", "nz");
+                document.getElementById("slide").classList.add("ko", "fu", "fi", "ms", "v", "ot", "fk", "mu", "nz");
+                status = "over300";
+            }
+        } else if(x <= 300) {
+            if(status == "over300") {
+                document.getElementById("slide").classList.remove("ko", "fu", "fi", "ms", "v", "ot", "fk", "mu", "nz");
+                document.getElementById("slide").classList.add("ko", "fu", "fq", "ms", "v", "ny", "fk", "mu", "nz");
+                status = "under300";
+            }
+        }
+    })
+})
