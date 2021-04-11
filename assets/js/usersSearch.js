26ae5ccf4e7ddb78c7addb176c38dc40505747d7
story <70672236+quicklooked@users.noreply.github.com>
2021-04-11 17:36:01 +0700

    Update usersSearch.js

--- assets/js/usersSearch.js
+++ assets/js/usersSearch.js
@@ -4,12 +4,12 @@ function usersSearch() {
 		document.getElementById("search").tabIndex = 0;
 		document.getElementById("search").ariaHidden = "false";
 		document.getElementById("search").classList.remove("ej", "ef", "ek", "cq", "av", "el", "em", "en", "eo", "ag", "ep", "eq", "er");
-		document.getElementById("search").classList.add("ej", "ef", "ek", "cq", "av", "or", "em", "en", "eo", "ag", "os", "ot", "er");
+		document.getElementById("search").classList.add("ej", "ef", "ek", "cq", "av", "or", "em", "en", "eo", "ag", "os", "oti", "er");
 		document.getElementById("search").focus();
 	} else {
 		document.getElementById("search").tabIndex = -1;
 		document.getElementById("search").ariaHidden = "true";
-		document.getElementById("search").classList.remove("ej", "ef", "ek", "cq", "av", "or", "em", "en", "eo", "ag", "os", "ot", "er");
+		document.getElementById("search").classList.remove("ej", "ef", "ek", "cq", "av", "or", "em", "en", "eo", "ag", "os", "oti", "er");
 		document.getElementById("search").classList.add("ej", "ef", "ek", "cq", "av", "el", "em", "en", "eo", "ag", "ep", "eq", "er");
 	}
 }
