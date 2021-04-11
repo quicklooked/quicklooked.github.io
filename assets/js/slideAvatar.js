dfeebc1449a2c886db404f185c85dd4b3b30e33d
story <70672236+quicklooked@users.noreply.github.com>
2021-04-11 16:56:30 +0700

    Update slideAvatar.js

--- assets/js/slideAvatar.js
+++ assets/js/slideAvatar.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var status = "under179";
+    var status = "under1";
     window.addEventListener("scroll", function() {
         var x = pageYOffset;
-        if(x > 179) {
-            if(status == "under179") {
+        if(x > 1) {
+            if(status == "under1") {
                 document.getElementById("slide").classList.remove("ko", "fu", "fq", "ms", "v", "ny", "fk", "mu", "nz");
                 document.getElementById("slide").classList.add("ko", "fu", "fi", "ms", "v", "ot", "fk", "mu", "nz");
-                status = "over179";
+                status = "over1";
             }
-        } else if(x <= 179) {
-            if(status == "over179") {
+        } else if(x <= 1) {
+            if(status == "over1") {
                 document.getElementById("slide").classList.remove("ko", "fu", "fi", "ms", "v", "ot", "fk", "mu", "nz");
                 document.getElementById("slide").classList.add("ko", "fu", "fq", "ms", "v", "ny", "fk", "mu", "nz");
-                status = "under179";
+                status = "under1";
             }
         }
     })
