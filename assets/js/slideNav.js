e6a8480055d86a1074bbfc3fba65bf7e33924a77
story <70672236+quicklooked@users.noreply.github.com>
2021-04-11 18:16:35 +0700

    Update slideNav.js

--- assets/js/slideNav.js
+++ assets/js/slideNav.js
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded", function() {
         if(x > 0) {
             if(status == "under0") {
                 document.getElementById("slideNav").classList.remove("fd", "fe", "ff", "fg", "c", "fh", "el", "fi", "fj", "fk", "ir", "ah", "fm");
-                document.getElementById("slideNav").classList.add("fd" "fe" "ff" "fg" "c" "fh" "ko" "fi" "fj" "fk" "ir" "ah" "owi");
+                document.getElementById("slideNav").classList.add("fd", "fe", "ff", "fg", "c", "fh", "ko", "fi", "fj", "fk", "ir", "ah", "owi");
                 status = "over0";
             }
         } else if(x <= 0) {
             if(status == "over0") {
-                document.getElementById("slideNav").classList.remove("fd" "fe" "ff" "fg" "c" "fh" "ko" "fi" "fj" "fk" "ir" "ah" "owi");
+                document.getElementById("slideNav").classList.remove("fd", "fe", "ff", "fg", "c", "fh", "ko", "fi", "fj", "fk", "ir", "ah", "owi");
                 document.getElementById("slideNav").classList.add("fd", "fe", "ff", "fg", "c", "fh", "el", "fi", "fj", "fk", "ir", "ah", "fm");
                 status = "under0";
             }
