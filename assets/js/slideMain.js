a653b2e129d4507f48f5d457c1e577ae1362bb19
story <70672236+quicklooked@users.noreply.github.com>
2021-04-11 19:52:36 +0700

    Update slideMain.js

--- assets/js/slideMain.js
+++ assets/js/slideMain.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     var status = "under552";
     window.addEventListener("scroll", function() {
         var x = screen.width;
-        if(x > 552) {
+        if(x < 552) {
             if(status == "under552") {
                 document.getElementById("slideMain").classList.remove("z", "ix", "ai", "aj", "ak", "c", "iy", "iz");
                 document.getElementById("slideMain").classList.add("z", "ah", "ai", "aj", "ak", "c", "iy", "ma", "mbb", "iz");
                 status = "over552";
             }
-        } else if(x <= 552) {
+        } else if(x >= 552) {
             if(status == "over552") {
                 document.getElementById("slideMain").classList.remove("z", "ah", "ai", "aj", "ak", "c", "iy", "ma", "mbb", "iz");
                 document.getElementById("slideMain").classList.add("z", "ix", "ai", "aj", "ak", "c", "iy", "iz");
