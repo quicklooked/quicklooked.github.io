ce4f8dc4f17f50101e095bb7cc7dcc66dafbdec0
story <70672236+quicklooked@users.noreply.github.com>
2021-04-13 18:51:52 +0700

    Update navChange.js

--- assets/js/navChange.js
+++ assets/js/navChange.js
@@ -12,6 +12,7 @@ function navChange() {
 		svg_element.setAttribute('d', new_path);
 		document.getElementById("navchange3").classList.remove("onx", "ai", "mnx");
 		document.getElementById("navchange3").classList.add("onx", "ai");
+		document.getElementById("navchange1").focus();
 	} else {
 		document.getElementById("navchange").ariaLabel = "Expand navbar";
 		document.getElementById("navchange1").classList.remove("cm", "mfx", "mgx");
