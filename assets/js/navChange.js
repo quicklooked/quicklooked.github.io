bfd8d8c9d8774333e150b5398d6674ad223816c2
story <70672236+quicklooked@users.noreply.github.com>
2021-04-13 18:04:27 +0700

    Update navChange.js

--- assets/js/navChange.js
+++ assets/js/navChange.js
@@ -1,16 +1,19 @@
 function navChange() {
 	var attr = document.getElementById("navchange").attributes;
+	var attr1 = document.getElementById("navchange1").attributes;
+	var attr2 = document.getElementById("navchange2").attributes;
+	var attr3 = document.getElementById("navchange3").attributes;
 	if(attr['aria-label'].value == "Expand navbar") {
-		document.getElementById("navchange1").ariaLabel = "Collapse navbar";
-		document.getElementById("navchange2").classList.remove("ggx");
-		document.getElementById("navchange2").classList.add("cm", "mfx", "mgx");
-		document.getElementById("navchange4").classList.remove("onx", "ai", "mnx");
-		document.getElementById("navchange4").classList.add("onx", "ai");
+		document.getElementById("navchange").ariaLabel = "Collapse navbar";
+		document.getElementById("navchange1").classList.remove("ggx");
+		document.getElementById("navchange1").classList.add("cm", "mfx", "mgx");
+		document.getElementById("navchange3").classList.remove("onx", "ai", "mnx");
+		document.getElementById("navchange3").classList.add("onx", "ai");
 	} else {
-		document.getElementById("navchange1").ariaLabel = "Expand navbar";
-		document.getElementById("navchange2").classList.remove("cm", "mfx", "mgx");
-		document.getElementById("navchange2").classList.add("ggx");
-		document.getElementById("navchange4").classList.remove("onx", "ai");
-		document.getElementById("navchange4").classList.add("onx", "ai", "mnx");
+		document.getElementById("navchange").ariaLabel = "Expand navbar";
+		document.getElementById("navchange1").classList.remove("cm", "mfx", "mgx");
+		document.getElementById("navchange1").classList.add("ggx");
+		document.getElementById("navchange3").classList.remove("onx", "ai");
+		document.getElementById("navchange3").classList.add("onx", "ai", "mnx");
 	}
 }
