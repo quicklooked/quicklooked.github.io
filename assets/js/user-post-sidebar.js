1efd9fc0b48111345e598b08a5dae008be442df2
story <70672236+quicklooked@users.noreply.github.com>
2021-04-14 23:31:18 +0700

    Update user-post-sidebar.js

--- assets/js/user-post-sidebar.js
+++ assets/js/user-post-sidebar.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var status = "under0";
+    var status = "under1339";
     window.addEventListener("scroll", function() {
         var x = pageYOffset;
-        if(x > 0) {
-            if(status == "under0") {
+        if(x > 1339) {
+            if(status == "under1339") {
                 document.getElementById("slide").classList.remove("em", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
                 document.getElementById("slide").classList.add("nr", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
                 document.getElementById("slide1").classList.remove("gg");
                 document.getElementById("slide1").classList.add("vas");
                 document.getElementById("slide2").classList.remove("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
                 document.getElementById("slide2").classList.add("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
-                status = "over0";
+                status = "over1339";
             }
-        } else if(x <= 0) {
-            if(status == "over0") {
+        } else if(x <= 1339) {
+            if(status == "over1339") {
                 document.getElementById("slide").classList.remove("nr", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
                 document.getElementById("slide").classList.add("em", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
                 document.getElementById("slide1").classList.remove("vas");
                 document.getElementById("slide1").classList.add("gg");
                 document.getElementById("slide2").classList.remove("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
                 document.getElementById("slide2").classList.add("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
-                status = "under0";
+                status = "under1339";
             }
         }
     })
