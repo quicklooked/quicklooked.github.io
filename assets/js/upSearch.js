822407d4c9c65c41ed47ae06204ea56838b2a620
story <70672236+quicklooked@users.noreply.github.com>
2021-04-15 00:52:09 +0700

    Update upSearch.js

--- assets/js/upSearch.js
+++ assets/js/upSearch.js
@@ -5,13 +5,15 @@ function upSearch() {
 		document.getElementById("search").ariaHidden = "false";
 		document.getElementById("search").classList.remove("ek", "eg", "el", "cr", "aw", "em", "en", "eo", "ep", "ah", "eq", "er", "es");
 		document.getElementById("search").classList.add("ek", "eg", "el", "cr", "aw", "atf", "en", "eo", "ep", "ah", "atg", "afw", "es");
-    document.getElementById("search").style.fontFamily = "sohne";
-    document.getElementById("myForm").autocomplete = "off";
+		document.getElementById("search").style.fontFamily = "sohne";
+		document.getElementById("search").autocomplete = "off";
 		document.getElementById("search").focus();
 	} else {
 		document.getElementById("search").tabIndex = -1;
 		document.getElementById("search").ariaHidden = "true";
 		document.getElementById("search").classList.remove("ek", "eg", "el", "cr", "aw", "atf", "en", "eo", "ep", "ah", "atg", "afw", "es");
 		document.getElementById("search").classList.add("ek", "eg", "el", "cr", "aw", "em", "en", "eo", "ep", "ah", "eq", "er", "es");
+		document.getElementById("search").style.fontFamily = "system-ui";
+		document.getElementById("search").autocomplete = "off";
 	}
 }
