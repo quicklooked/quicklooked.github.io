e3fc3db704eba937c2b45d1568ab6c00b9a4e53b
story <70672236+quicklooked@users.noreply.github.com>
2021-04-15 00:46:39 +0700

    Create upSearch.js

---
+++ assets/js/upSearch.js
@@ -0,0 +1,17 @@
+function upSearch() {
+	var attr = document.getElementById("search").attributes;
+	if(attr['aria-hidden'].value == "true") {
+		document.getElementById("search").tabIndex = 0;
+		document.getElementById("search").ariaHidden = "false";
+		document.getElementById("search").classList.remove("ek", "eg", "el", "cr", "aw", "em", "en", "eo", "ep", "ah", "eq", "er", "es");
+		document.getElementById("search").classList.add("ek", "eg", "el", "cr", "aw", "atf", "en", "eo", "ep", "ah", "atg", "afw", "es");
+    document.getElementById("search").style.fontFamily = "sohne";
+    document.getElementById("myForm").autocomplete = "off";
+		document.getElementById("search").focus();
+	} else {
+		document.getElementById("search").tabIndex = -1;
+		document.getElementById("search").ariaHidden = "true";
+		document.getElementById("search").classList.remove("ek", "eg", "el", "cr", "aw", "atf", "en", "eo", "ep", "ah", "atg", "afw", "es");
+		document.getElementById("search").classList.add("ek", "eg", "el", "cr", "aw", "em", "en", "eo", "ep", "ah", "eq", "er", "es");
+	}
+}
