3f01a9b85a63c8ae22c2f433ff83a2a7919ebf37
story <70672236+quicklooked@users.noreply.github.com>
2021-04-14 23:23:59 +0700

    Update user-post-sidebar.js

--- assets/js/user-post-sidebar.js
+++ assets/js/user-post-sidebar.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 document.getElementById("slide").classList.remove("em", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
                 document.getElementById("slide").classList.add("nr", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
                 document.getElementById("slide1").classList.remove("gg");
-                document.getElementById("slide1").classList.add("va");
+                document.getElementById("slide1").classList.add("vas");
                 document.getElementById("slide2").classList.remove("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
                 document.getElementById("slide2").classList.add("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
                 status = "over0";
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
             if(status == "over0") {
                 document.getElementById("slide").classList.remove("nr", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
                 document.getElementById("slide").classList.add("em", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
-                document.getElementById("slide1").classList.remove("va");
+                document.getElementById("slide1").classList.remove("vas");
                 document.getElementById("slide1").classList.add("gg");
                 document.getElementById("slide2").classList.remove("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
                 document.getElementById("slide2").classList.add("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
