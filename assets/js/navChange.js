8451f332e7334faf2dcbe31caf9fdecb84350d77
story <70672236+quicklooked@users.noreply.github.com>
2021-04-14 09:48:21 +0700

    Update navChange.js

--- assets/js/navChange.js
+++ assets/js/navChange.js
@@ -5,6 +5,7 @@ function navChange() {
 	var attr3 = document.getElementById("navchange3").attributes;
 	if(attr['aria-label'].value == "Expand navbar") {
 		document.getElementById("navchange").ariaLabel = "Collapse navbar";
+		document.getElementById("navchange").style.outline = "none";
 		document.getElementById("navchange1").classList.remove("ggx");
 		document.getElementById("navchange1").classList.add("cm", "mfx", "mgx");
 		new_path = 'M12.6.59L7 6.19 1.39.6l-.8.8L6.19 7 .6 12.61l.8.8L7 7.81l5.61 5.6.8-.8L7.81 7l5.6-5.61';
@@ -15,6 +16,7 @@ function navChange() {
 		document.getElementById("navchange3").focus();
 	} else {
 		document.getElementById("navchange").ariaLabel = "Expand navbar";
+		document.getElementById("navchange").style.outline = "inherit";
 		document.getElementById("navchange1").classList.remove("cm", "mfx", "mgx");
 		document.getElementById("navchange1").classList.add("ggx");
 		new_path = 'M0 .5h14M0 7h14M0 13.5h14';
