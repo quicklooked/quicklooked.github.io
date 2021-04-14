7f3dbb126234cdfa2d63d2b196ce47124932b2f2
story <70672236+quicklooked@users.noreply.github.com>
2021-04-14 23:55:59 +0700

    Update user-post-sidebar.js

--- assets/js/user-post-sidebar.js
+++ assets/js/user-post-sidebar.js
@@ -1,27 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var status = "under1239";
+    var status = "under1180";
     var clientHeight = document.getElementById("divSlide").clientHeight;
     window.addEventListener("scroll", function() {
         var x = pageYOffset;
-        if(x > 1239) {
-            if(status == "under1239") {
+        if(x > 1180) {
+            if(status == "under1180") {
                 document.getElementById("slide").classList.remove("em", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
                 document.getElementById("slide").classList.add("nr", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
                 document.getElementById("slide1").classList.remove("gg");
                 document.getElementById("slide1").classList.add("vas");
                 document.getElementById("slide2").classList.remove("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
                 document.getElementById("slide2").classList.add("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
-                status = "over1239";
+                status = "over1180";
             }
-        } else if(x <= 1239 + clientHeight) {
-            if(status == "over1239") {
+        } else if(x <= 1180 + clientHeight) {
+            if(status == "over1180") {
                 document.getElementById("slide").classList.remove("nr", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
                 document.getElementById("slide").classList.add("em", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
                 document.getElementById("slide1").classList.remove("vas");
                 document.getElementById("slide1").classList.add("gg");
                 document.getElementById("slide2").classList.remove("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
                 document.getElementById("slide2").classList.add("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
-                status = "under1239";
+                status = "under1180";
             }
         }
     })
