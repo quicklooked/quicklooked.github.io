14828ff5fc5a00b1cb85c13f9fb7d781880a8c0e
story <70672236+quicklooked@users.noreply.github.com>
2021-04-16 15:35:48 +0700

    Update play_video.js

--- assets/js/play_video.js
+++ assets/js/play_video.js
@@ -8,9 +8,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const vimeo = { referrerPolicy: 'no-referrer' }
   const fullscreen = { enabled: true , fallback: true , iosNative: false, container: '#container' }
   const speed =  { selected: 1, options: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2] }
-  const player = new Plyr('#player', { debug: true , title: 'Quicklook Play Video' , iconUrl: 'https://cdn.jsdelivr.net/gh/theprojectsomething/plyr@a55bf00/demo/dist/demo.svg' , controls , settings , i18n , speed , keyboard , tooltips , captions , vimeo , fullscreen });
+  const player = new Plyr('#player', { debug: true , title: 'Quicklook Play Video' , iconUrl: 'https://cdn.jsdelivr.net/gh/theprojectsomething/plyr@a55bf00/demo/dist/demo.svg' , controls , keyboard , tooltips , fullscreen });
   window.player = player;
-  player.volume = 0.5;
+  player.volume = 1;
   function on(selector, type, callback) {
     document.querySelector(selector).addEventListener(type, callback, false);
   }
