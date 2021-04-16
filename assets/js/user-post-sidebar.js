a00a99096e19ae5918940b69b3ec75fdb7ecb1bd
story <70672236+quicklooked@users.noreply.github.com>
2021-04-17 00:07:22 +0700

    Update user-post-sidebar.js

--- assets/js/user-post-sidebar.js
+++ assets/js/user-post-sidebar.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var clientHeight = document.getElementById("divSlide").clientHeight;
   var clientHeight1 = document.getElementById("divSlide1").clientHeight;
   var clientHeight2 = document.getElementById("divSlide2").clientHeight;
-  var disableslibar = document.getElementById("disableslibar");
+  var disableslibar = document.getElementById("disableslibar")[0];
   window.addEventListener("scroll", function() {
     var x = pageYOffset;
     var y = 88 + divSlide1.clientHeight + divSlide2.clientHeight;
