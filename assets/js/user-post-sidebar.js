23cbd72f7fd76167a8c157325058a5b862cfb7f1
story <70672236+quicklooked@users.noreply.github.com>
2021-04-17 00:08:10 +0700

    Update user-post-sidebar.js

--- assets/js/user-post-sidebar.js
+++ assets/js/user-post-sidebar.js
@@ -4,10 +4,11 @@ document.addEventListener("DOMContentLoaded", function() {
   var clientHeight1 = document.getElementById("divSlide1").clientHeight;
   var clientHeight2 = document.getElementById("divSlide2").clientHeight;
   var disableslibar = document.getElementById("disableslibar")[0];
+  var idsl = disableslibar.id;
   window.addEventListener("scroll", function() {
     var x = pageYOffset;
     var y = 88 + divSlide1.clientHeight + divSlide2.clientHeight;
-    if(x > y && x < divSlide.clientHeight && disableslibar.id != disableslibar) {
+    if(x > y && x < divSlide.clientHeight && idsl.id != disableslibar) {
       if(status == "under1180") {
         document.getElementById("slide").classList.remove("em", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
         document.getElementById("slide").classList.add("nr", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
