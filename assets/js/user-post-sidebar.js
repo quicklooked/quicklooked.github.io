8e0157e813d302f530d6b0106c2e700755168ce3
story <70672236+quicklooked@users.noreply.github.com>
2021-04-16 21:52:34 +0700

    Update user-post-sidebar.js

--- assets/js/user-post-sidebar.js
+++ assets/js/user-post-sidebar.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener("scroll", function() {
     var x = pageYOffset;
     var y = 88 + divSlide1.clientHeight + divSlide2.clientHeight;
-    if(x > y && x < divSlide.clientHeight | slideWidth.clientWidth < slideWidth1.clientWidth) {
+    if(x > y && x < divSlide.clientHeight && slideWidth.clientWidth < slideWidth1.clientWidth) {
       if(status == "under1180") {
         document.getElementById("slide").classList.remove("em", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
         document.getElementById("slide").classList.add("nr", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
