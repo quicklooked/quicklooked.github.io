5a91e48cb4d4c82fa7d53342ea4872a04e2c4e8c
story <70672236+quicklooked@users.noreply.github.com>
2021-04-17 20:47:09 +0700

    Update highlight.js

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,8 +1,10 @@
 var a = document.getElementById("highlight").clientHeight;
 var b = document.getElementById("highlight").offsetTop;
+var x = window.clientWidth - 680;
 var c = b - (a / 2);
 if(a > 0) {
   document.getElementById("highlight2").style.top = c + "px";
+  document.getElementById("highlight1").style.width = x + "px";
 } else {
   document.getElementById("highlight3").style.display = "none";
 }
