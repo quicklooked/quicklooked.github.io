95ac5c31d2e9e7fb65d051427e62d814afdbb631
story <70672236+quicklooked@users.noreply.github.com>
2021-04-18 16:54:34 +0700

    Update cite.js

--- assets/js/cite.js
+++ assets/js/cite.js
@@ -1,10 +1,10 @@
-$('#cite').magnificPopup({
+$('#inline-popups').magnificPopup({
   delegate: 'a',
-  removalDelay: 1,
+  removalDelay: 200, //delay removal by X to allow out-animation
   callbacks: {
     beforeOpen: function() {
-      this.st.mainClass = this.st.el.attr('data-effect');
+       this.st.mainClass = this.st.el.attr('data-effect');
     }
   },
-  midClick: true
+  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
 });
