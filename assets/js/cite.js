981a534910fd0d5fd8aa5d0426bb6b6787dfc6d1
story <70672236+quicklooked@users.noreply.github.com>
2021-04-18 17:09:13 +0700

    Update cite.js

--- assets/js/cite.js
+++ assets/js/cite.js
@@ -8,4 +8,3 @@ $('#cite').magnificPopup({
   },
   midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
 });
-pass $ in $(document).ready(function ($) 
