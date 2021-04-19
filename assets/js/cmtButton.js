9819afc58939cb37c73c3baf2cf4bc352c1ecc80
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-20 01:22:34 +0700

    Update cmtButton.js

--- assets/js/cmtButton.js
+++ assets/js/cmtButton.js
@@ -1,12 +1,12 @@
 function cmtButton() {
 	var x = document.getElementById("cmtButton");
 	var y = document.getElementById("cmtButton1");
-	if(attr['aria-label'].value == "close") {
+	if(attr['aria-label'].value == "open") {
 		x.style.display = "block";
-		document.getElementById("cmtButton1").ariaLabel = "open";
+		document.getElementById("cmtButton1").ariaLabel = "close";
 	}
 	else {
 		x.style.display = "none";
-		document.getElementById("cmtButton1").ariaLabel = "close";
+		document.getElementById("cmtButton1").ariaLabel = "open";
 	}
 }
