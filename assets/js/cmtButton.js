6c2ab10a3d93cbd5ac680dee53c8bceeb4a89a4a
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-20 01:42:36 +0700

    Update cmtButton.js

--- assets/js/cmtButton.js
+++ assets/js/cmtButton.js
@@ -1,7 +1,7 @@
 function cmtButton() {
 	var x = document.getElementById("cmtButton");
 	var attr = document.getElementById("cmtButton1");
-	if(attr['aria-label'].value = "open") {
+	if(document.getElementById("cmtButton1").ariaLabel = "open") {
 		x.style.display = "block";
 		document.getElementById("cmtButton1").ariaLabel = "close";
 	}
