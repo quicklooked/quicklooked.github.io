2fab6b7b6e3fcae52b048c7d29f9160cefad1bac
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-21 15:36:55 +0700

    Update audio.js

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,250 +1,261 @@
-$(function () {
-  var playerTrack = $("#player-track"),
-    bgArtwork = $("#bg-artwork"),
-    bgArtworkUrl,
-    albumName = $("#album-name"),
-    trackName = $("#track-name"),
-    albumArt = $("#album-art"),
-    sArea = $("#s-area"),
-    seekBar = $("#seek-bar"),
-    trackTime = $("#track-time"),
-    insTime = $("#ins-time"),
-    sHover = $("#s-hover"),
-    playPauseButton = $("#play-pause-button"),
-    i = playPauseButton.find("i"),
-    tProgress = $("#current-time"),
-    tTime = $("#track-length"),
-    seekT,
-    seekLoc,
-    seekBarPos,
-    cM,
-    ctMinutes,
-    ctSeconds,
-    curMinutes,
-    curSeconds,
-    durMinutes,
-    durSeconds,
-    playProgress,
-    bTime,
-    nTime = 0,
-    buffInterval = null,
-    tFlag = false,
-    albums = [
-      "Dawn",
-      "Me & You",
-      "Electro Boy",
-      "Home",
-      "Proxy (Original Mix)"
-    ],
-    trackNames = [
-      "Skylike - Dawn",
-      "Alex Skrindo - Me & You",
-      "Kaaze - Electro Boy",
-      "Jordan Schor - Home",
-      "Martin Garrix - Proxy"
-    ],
-    albumArtworks = ["_1", "_2", "_3", "_4", "_5"],
-    trackUrl = [
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/2.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/1.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/3.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/4.mp3",
-      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/5.mp3"
-    ],
-    playPreviousTrackButton = $("#play-previous"),
-    playNextTrackButton = $("#play-next"),
-    currIndex = -1;
+$(document).ready(function() {});
+$(window).on("load", function() {});
+$(window).resize(function() {});
 
-  function playPause() {
-    setTimeout(function () {
-      if (audio.paused) {
-        playerTrack.addClass("active");
-        albumArt.addClass("active");
-        checkBuffering();
-        i.attr("class", "fas fa-pause");
-        audio.play();
+function addAudioControls() {
+  $(".PodcastEpisode_Player").each(function() {
+    var playerID = $(this).find("audio").attr("id");
+    var audio = document.getElementById(playerID);
+    var isDragging = false;
+    var currentVolume = 1;
+    var isVolDragging = false;
+    audio.addEventListener("canplaythrough", function() {
+      var audioMinutes = audio.duration / 60;
+      if(audioMinutes.toFixed(2) != "NaN") {
+        $("#" + playerID).parent().find(".GNPAplayerControls_Time").html(audioMinutes.toFixed(2));
       } else {
-        playerTrack.removeClass("active");
-        albumArt.removeClass("active");
-        clearInterval(buffInterval);
-        albumArt.removeClass("buffering");
-        i.attr("class", "fas fa-play");
+        $("#" + playerID).parent().find(".GNPAplayerControls_Time").html("Loading");
+      }
+      $("#" + playerID).parent().find(".GNPAplayerControls_VolumeBarProg").css("width", audio.volume * 100 + "%");
+    }, false);
+    $(this).find(".GNPAplayerControls_Play").click(function(e) {
+      audio.play();
+      $(this).parent().find(".GNPAplayerControls_Play").css("display", "none");
+      $(this).parent().find(".GNPAplayerControls_Pause").css("display", "flex");
+      //console.log("Play1: "+$(this).parent().parent().parent().parent().find("audio").attr("id"));
+      appInsights.trackEvent("Podcast Play",
+        // String properties:
+        {
+          ID: TGNPageID,
+          URL: TGNPageURL,
+          AUDIO: audio.src,
+          USERID: currentUserUserName,
+          EMAIL: currentUserEmail,
+          CID: localStorage.getItem("currentUserCampaignQS")
+        },
+        // Numeric metrics:
+        {
+          UNIT: 1
+        });
+      e.stopImmediatePropagation();
+    });
+    $(this).find(".GNPAplayerControls_Pause").click(function(e) {
+      audio.pause();
+      $(this).parent().find(".GNPAplayerControls_Play").css("display", "flex");
+      $(this).parent().find(".GNPAplayerControls_Pause").css("display", "none");
+      //console.log("Pause1: "+$(this).parent().parent().parent().parent().find("audio").attr("id"));
+      appInsights.trackEvent("Podcast Pause",
+        // String properties:
+        {
+          ID: TGNPageID,
+          URL: TGNPageURL,
+          AUDIO: audio.src,
+          USERID: currentUserUserName,
+          EMAIL: currentUserEmail,
+          CID: localStorage.getItem("currentUserCampaignQS")
+        },
+        // Numeric metrics:
+        {
+          UNIT: 1
+        });
+      e.stopImmediatePropagation();
+    });
+    $(this).find(".GNPAplayerControls_ScrubBar").on("mousedown touchstart", function(e) {
+      //console.log("start");
+      $("DebugText").html();
+      isDragging = true;
+      var clickBarPercent = (e.clientX - 10 - $(this).offset().left) / $(this).width();
+      audio.currentTime = audio.duration * clickBarPercent;
+    });
+    $(this).find(".GNPAplayerControlsLeft").on("mouseenter mousemove", function(e) {
+      if(isDragging) {
+        //console.log("move");
         audio.pause();
+        $(this).parent().find(".GNPAplayerControls_Play").css("display", "flex");
+        $(this).parent().find(".GNPAplayerControls_Pause").css("display", "none");
+        var clickBarPercent = (e.clientX - 10 - $(this).find(".GNPAplayerControls_ScrubBar").offset().left) / $(this).find(".GNPAplayerControls_ScrubBar").width();
+        audio.currentTime = audio.duration * clickBarPercent;
       }
-    }, 300);
-  }
-
-  function showHover(event) {
-    seekBarPos = sArea.offset();
-    seekT = event.clientX - seekBarPos.left;
-    seekLoc = audio.duration * (seekT / sArea.outerWidth());
-
-    sHover.width(seekT);
-
-    cM = seekLoc / 60;
-
-    ctMinutes = Math.floor(cM);
-    ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
-
-    if (ctMinutes < 0 || ctSeconds < 0) return;
-
-    if (ctMinutes < 0 || ctSeconds < 0) return;
-
-    if (ctMinutes < 10) ctMinutes = "0" + ctMinutes;
-    if (ctSeconds < 10) ctSeconds = "0" + ctSeconds;
-
-    if (isNaN(ctMinutes) || isNaN(ctSeconds)) insTime.text("--:--");
-    else insTime.text(ctMinutes + ":" + ctSeconds);
-
-    insTime.css({ left: seekT, "margin-left": "-21px" }).fadeIn(0);
-  }
-
-  function hideHover() {
-    sHover.width(0);
-    insTime.text("00:00").css({ left: "0px", "margin-left": "0px" }).fadeOut(0);
-  }
-
-  function playFromClickedPos() {
-    audio.currentTime = seekLoc;
-    seekBar.width(seekT);
-    hideHover();
-  }
-
-  function updateCurrTime() {
-    nTime = new Date();
-    nTime = nTime.getTime();
-
-    if (!tFlag) {
-      tFlag = true;
-      trackTime.addClass("active");
-    }
-
-    curMinutes = Math.floor(audio.currentTime / 60);
-    curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
-
-    durMinutes = Math.floor(audio.duration / 60);
-    durSeconds = Math.floor(audio.duration - durMinutes * 60);
-
-    playProgress = (audio.currentTime / audio.duration) * 100;
-
-    if (curMinutes < 10) curMinutes = "0" + curMinutes;
-    if (curSeconds < 10) curSeconds = "0" + curSeconds;
-
-    if (durMinutes < 10) durMinutes = "0" + durMinutes;
-    if (durSeconds < 10) durSeconds = "0" + durSeconds;
-
-    if (isNaN(curMinutes) || isNaN(curSeconds)) tProgress.text("00:00");
-    else tProgress.text(curMinutes + ":" + curSeconds);
-
-    if (isNaN(durMinutes) || isNaN(durSeconds)) tTime.text("00:00");
-    else tTime.text(durMinutes + ":" + durSeconds);
-
-    if (
-      isNaN(curMinutes) ||
-      isNaN(curSeconds) ||
-      isNaN(durMinutes) ||
-      isNaN(durSeconds)
-    )
-      trackTime.removeClass("active");
-    else trackTime.addClass("active");
-
-    seekBar.width(playProgress + "%");
-
-    if (playProgress == 100) {
-      i.attr("class", "fa fa-play");
-      seekBar.width(0);
-      tProgress.text("00:00");
-      albumArt.removeClass("buffering").removeClass("active");
-      clearInterval(buffInterval);
-    }
-  }
-
-  function checkBuffering() {
-    clearInterval(buffInterval);
-    buffInterval = setInterval(function () {
-      if (nTime == 0 || bTime - nTime > 1000) albumArt.addClass("buffering");
-      else albumArt.removeClass("buffering");
-
-      bTime = new Date();
-      bTime = bTime.getTime();
-    }, 100);
-  }
-
-  function selectTrack(flag) {
-    if (flag == 0 || flag == 1) ++currIndex;
-    else --currIndex;
-
-    if (currIndex > -1 && currIndex < albumArtworks.length) {
-      if (flag == 0) i.attr("class", "fa fa-play");
-      else {
-        albumArt.removeClass("buffering");
-        i.attr("class", "fa fa-pause");
+    });
+    $(this).find(".GNPAplayerControlsLeft").on("mouseup mouseleave touchend", function(e) {
+      if(isDragging) {
+        var clickBarPercent = (e.clientX - 10 - $(this).find(".GNPAplayerControls_ScrubBar").offset().left) / $(this).find(".GNPAplayerControls_ScrubBar").width();
+        if(clickBarPercent > 0) {
+          audio.currentTime = audio.duration * clickBarPercent;
+        }
       }
-
-      seekBar.width(0);
-      trackTime.removeClass("active");
-      tProgress.text("00:00");
-      tTime.text("00:00");
-
-      currAlbum = albums[currIndex];
-      currTrackName = trackNames[currIndex];
-      currArtwork = albumArtworks[currIndex];
-
-      audio.src = trackUrl[currIndex];
-
-      nTime = 0;
-      bTime = new Date();
-      bTime = bTime.getTime();
-
-      if (flag != 0) {
-        audio.play();
-        playerTrack.addClass("active");
-        albumArt.addClass("active");
-
-        clearInterval(buffInterval);
-        checkBuffering();
+      isDragging = false;
+      //console.log("end");
+    });
+    $(this).find(".GNPAplayerControls_VolumeOn").click(function() {
+      document.getElementById(playerID).volume = 0;
+      $(this).parent().find(".GNPAplayerControls_VolumeOff").css("display", "flex");
+      $(this).parent().find(".GNPAplayerControls_VolumeOn").css("display", "none");
+      $(this).parent().find(".GNPAplayerControls_VolumeBarProg").css("width", "2%");
+    });
+    $(this).find(".GNPAplayerControls_VolumeOff").click(function() {
+      document.getElementById(playerID).volume = currentVolume;
+      $(this).parent().find(".GNPAplayerControls_VolumeOff").css("display", "none");
+      $(this).parent().find(".GNPAplayerControls_VolumeOn").css("display", "flex");
+      $(this).parent().find(".GNPAplayerControls_VolumeBarProg").css("width", "100%");
+    });
+    $(this).find(".GNPAplayerControls_VolumeBar").on("mousedown touchstart", function(e) {
+      //console.log("Vstart");
+      isVolDragging = true;
+      var clickVolBar = (e.clientX - $(this).offset().left) / $(this).width();
+      var clickVolBarPercent = Math.ceil(
+        ((e.clientX - $(this).offset().left) / $(this).width()) * 100);
+      if(clickVolBar < 0) {
+        clickVolBar = 0;
       }
+      if(clickVolBar > 1) {
+        clickVolBar = 1;
+      }
+      audio.volume = clickVolBar;
+      currentVolume = clickVolBar;
+      $(this).parent().find(".GNPAplayerControls_VolumeBarProg").css("width", clickVolBarPercent + "%");
+      if(clickVolBarPercent > 5) {
+        $(this).parent().find(".GNPAplayerControls_VolumeOff").css("display", "none");
+        $(this).parent().find(".GNPAplayerControls_VolumeOn").css("display", "flex");
+      } else {
+        $(this).parent().find(".GNPAplayerControls_VolumeOff").css("display", "flex");
+        $(this).parent().find(".GNPAplayerControls_VolumeOn").css("display", "none");
+        $(this).parent().find(".GNPAplayerControls_VolumeBarProg").css("width", "2%");
+      }
+    });
+    $(this).find(".GNPAplayerControls_Volume").on("mouseenter mousemove", function(e) {
+      if(isVolDragging) {
+        //console.log("Vmove");
+        var clickVolBar = (e.clientX - $(this).find(".GNPAplayerControls_VolumeBar").offset().left) / $(this).find(".GNPAplayerControls_VolumeBar").width();
+        var clickVolBarPercent = Math.ceil(
+          ((e.clientX - $(this).find(".GNPAplayerControls_VolumeBar").offset().left) / $(this).find(".GNPAplayerControls_VolumeBar").width()) * 100);
+        if(clickVolBar < 0) {
+          clickVolBar = 0;
+        }
+        if(clickVolBar > 1) {
+          clickVolBar = 1;
+        }
+        audio.volume = clickVolBar;
+        currentVolume = clickVolBar;
+        $(this).find(".GNPAplayerControls_VolumeBarProg").css("width", clickVolBarPercent + "%");
+        if(clickVolBarPercent > 5) {
+          $(this).find(".GNPAplayerControls_VolumeOff").css("display", "none");
+          $(this).find(".GNPAplayerControls_VolumeOn").css("display", "flex");
+        } else {
+          $(this).find(".GNPAplayerControls_VolumeOff").css("display", "flex");
+          $(this).find(".GNPAplayerControls_VolumeOn").css("display", "none");
+          $(this).find(".GNPAplayerControls_VolumeBarProg").css("width", "2%");
+        }
+      }
+    });
+    $(this).find(".GNPAplayerControls_Volume").on("mouseup mouseleave touchend", function(e) {
+      if(isVolDragging) {
+        var clickVolBar = (e.clientX - $(this).find(".GNPAplayerControls_VolumeBar").offset().left) / $(this).find(".GNPAplayerControls_VolumeBar").width();
+        var clickVolBarPercent = Math.ceil(
+          ((e.clientX - $(this).find(".GNPAplayerControls_VolumeBar").offset().left) / $(this).find(".GNPAplayerControls_VolumeBar").width()) * 100);
+        if(clickVolBar < 0) {
+          clickVolBar = 0;
+        }
+        if(clickVolBar > 1) {
+          clickVolBar = 1;
+        }
+        audio.volume = clickVolBar;
+        currentVolume = clickVolBar;
+        $(this).find(".GNPAplayerControls_VolumeBarProg").css("width", clickVolBarPercent + "%");
+        if(clickVolBarPercent > 5) {
+          $(this).find(".GNPAplayerControls_VolumeOff").css("display", "none");
+          $(this).find(".GNPAplayerControls_VolumeOn").css("display", "flex");
+        } else {
+          $(this).find(".GNPAplayerControls_VolumeOff").css("display", "flex");
+          $(this).find(".GNPAplayerControls_VolumeOn").css("display", "none");
+          $(this).find(".GNPAplayerControls_VolumeBarProg").css("width", "2%");
+        }
+      }
+      isVolDragging = false;
+      //console.log("Vend");
+    });
+  });
+  int = window.setInterval(updateProgress, 300);
 
-      albumName.text(currAlbum);
-      trackName.text(currTrackName);
-      albumArt.find("img.active").removeClass("active");
-      $("#" + currArtwork).addClass("active");
-
-      bgArtworkUrl = $("#" + currArtwork).attr("src");
-
-      bgArtwork.css({ "background-image": "url(" + bgArtworkUrl + ")" });
-    } else {
-      if (flag == 0 || flag == 1) --currIndex;
-      else ++currIndex;
-    }
+  function updateProgress() {
+    $(".PodcastEpisode_Player").each(function() {
+      var playerID = $(this).find("audio").attr("id");
+      var audio = document.getElementById(playerID);
+      progress_seconds = audio.currentTime;
+      progress_percent = Math.ceil((audio.currentTime / audio.duration) * 100);
+      var audioMinutes = progress_seconds / 60;
+      var audioMinutesTotal = audio.duration / 60;
+      $("#" + playerID).parent().find(".GNPAplayerControls_TimeNow").html(audioMinutes.toFixed(2));
+      $("#" + playerID).parent().find(".GNPAplayerControls_ScrubBarProg").css("width", progress_percent + "%");
+      //10% tracking
+      if($("#" + playerID).parent().find(".GNPAplayerControls_ScrubBarProg").attr("data-playstatus") == "unplayed" && progress_percent > 10) {
+        $("#" + playerID).parent().find(".GNPAplayerControls_ScrubBarProg").attr("data-playstatus", "10");
+        appInsights.trackEvent("Podcast Play 10",
+          // String properties:
+          {
+            ID: TGNPageID,
+            URL: TGNPageURL,
+            AUDIO: audio.src,
+            USERID: currentUserUserName,
+            EMAIL: currentUserEmail,
+            CID: localStorage.getItem("currentUserCampaignQS")
+          },
+          // Numeric metrics:
+          {
+            UNIT: 1
+          });
+      }
+      //50% tracking
+      if($("#" + playerID).parent().find(".GNPAplayerControls_ScrubBarProg").attr("data-playstatus") == "10" && progress_percent > 50) {
+        $("#" + playerID).parent().find(".GNPAplayerControls_ScrubBarProg").attr("data-playstatus", "50");
+        appInsights.trackEvent("Podcast Play 50",
+          // String properties:
+          {
+            ID: TGNPageID,
+            URL: TGNPageURL,
+            AUDIO: audio.src,
+            USERID: currentUserUserName,
+            EMAIL: currentUserEmail,
+            CID: localStorage.getItem("currentUserCampaignQS")
+          },
+          // Numeric metrics:
+          {
+            UNIT: 1
+          });
+      }
+      //90% tracking
+      if($("#" + playerID).parent().find(".GNPAplayerControls_ScrubBarProg").attr("data-playstatus") == "50" && progress_percent > 90) {
+        $("#" + playerID).parent().find(".GNPAplayerControls_ScrubBarProg").attr("data-playstatus", "90");
+        appInsights.trackEvent("Podcast Play 90",
+          // String properties:
+          {
+            ID: TGNPageID,
+            URL: TGNPageURL,
+            AUDIO: audio.src,
+            USERID: currentUserUserName,
+            EMAIL: currentUserEmail,
+            CID: localStorage.getItem("currentUserCampaignQS")
+          },
+          // Numeric metrics:
+          {
+            UNIT: 1
+          });
+      }
+      if(audioMinutesTotal.toFixed(2) != "NaN") {
+        $("#" + playerID).parent().find(".GNPAplayerControls_Time").html(audioMinutesTotal.toFixed(2));
+      } else {
+        $("#" + playerID).parent().find(".GNPAplayerControls_Time").html("Loading");
+      }
+      $("#" + playerID).parent().find(".GNPAplayerControls_VolumeBarProg").css("width", audio.volume * 100 + "%");
+      if(audio.duration > 0 && !audio.paused) {
+        //Its playing...do your job
+        $(this).parent().find(".GNPAplayerControls_Play").css("display", "none");
+        $(this).parent().find(".GNPAplayerControls_Pause").css("display", "flex");
+      } else {
+        //Not playing...maybe paused, stopped or never played.
+        $(this).parent().find(".GNPAplayerControls_Play").css("display", "flex");
+        $(this).parent().find(".GNPAplayerControls_Pause").css("display", "none");
+      }
+    });
   }
-
-  function initPlayer() {
-    audio = new Audio();
-
-    selectTrack(0);
-
-    audio.loop = false;
-
-    playPauseButton.on("click", playPause);
-
-    sArea.mousemove(function (event) {
-      showHover(event);
-    });
-
-    sArea.mouseout(hideHover);
-
-    sArea.on("click", playFromClickedPos);
-
-    $(audio).on("timeupdate", updateCurrTime);
-
-    playPreviousTrackButton.on("click", function () {
-      selectTrack(-1);
-    });
-    playNextTrackButton.on("click", function () {
-      selectTrack(1);
-    });
-  }
-
-  initPlayer();
-});
+}
