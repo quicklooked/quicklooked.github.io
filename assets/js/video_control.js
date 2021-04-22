cec33714c3788df229feea1a2f024ab98ed00286
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-23 03:17:40 +0700

    Update video_control.js

--- assets/js/video_control.js
+++ assets/js/video_control.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => { 
-  const controls = ["play", "mute"]
+  const controls = ["mute", 'captions', 'current-time', 'progress', 'fullscreen']
   const settings = ['speed']
   const i18n = { speed: 'Speed' }
   const keyboard = { global: true }
