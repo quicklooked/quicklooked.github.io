8ea1029eb48b3e2beeb3e8c807d98102296f4974
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-22 16:10:21 +0700

    Update video_control.js

--- assets/js/video_control.js
+++ assets/js/video_control.js
@@ -6,9 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const tooltips = { controls: true }
   const captions = { active: true }
   const vimeo = { referrerPolicy: 'no-referrer' }
-  const fullscreen = { enabled: true , fallback: true , iosNative: false }
+  const fullscreen = { enabled: true , fallback: true , iosNative: false, container: '#container'}
   const speed =  { selected: 1, options: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2] }
-  const player = new Plyr('#player', { debug: true , title: 'Quicklook Video Player' , iconUrl: '/assets/images/fit/32/1_m-R_BkNf1Qjr1YbyOIJY2w.png' , controls , settings , i18n , speed , keyboard , tooltips , captions , vimeo , fullscreen });
+  const player = new Plyr('#player', { debug: true , title: 'Quicklook Video Player' , iconUrl: 'https://cdn.jsdelivr.net/gh/theprojectsomething/plyr@a55bf00/demo/dist/demo.svg' , controls , settings , i18n , speed , keyboard , tooltips , captions , vimeo , fullscreen });
   window.player = player;
   player.volume = 0.5;
   function on(selector, type, callback) {
