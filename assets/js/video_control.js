d6379ddfce8757e20e4e8f00ab40b40bec25eabb
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-24 02:59:30 +0700

    Update video_control.js

--- assets/js/video_control.js
+++ assets/js/video_control.js
@@ -1,50 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => { 
   const controls = ["play", "progress", "current-time", "mute", "volume", "captions", "settings", "pip", "airplay", "fullscreen" ]
   const settings = ['speed', 'quality', 'caption']
-  const i18n = {
-    restart: 'Restart',
-    rewind: 'Rewind {seektime}s',
-    play: 'Play',
-    pause: 'Pause',
-    fastForward: 'Forward {seektime}s',
-    seek: 'Seek',
-    seekLabel: '{currentTime} of {duration}',
-    played: 'Played',
-    buffered: 'Buffered',
-    currentTime: 'Current time',
-    duration: 'Duration',
-    volume: 'Volume',
-    mute: 'Mute',
-    unmute: 'Unmute',
-    enableCaptions: 'Enable captions',
-    disableCaptions: 'Disable captions',
-    download: 'Download',
-    enterFullscreen: 'Enter fullscreen',
-    exitFullscreen: 'Exit fullscreen',
-    frameTitle: 'Player for {title}',
-    captions: 'Captions',
-    settings: 'Settings',
-    pip: 'PIP',
-    menuBack: 'Go back to previous menu',
-    speed: 'Speed',
-    normal: 'Normal',
-    quality: 'Quality',
-    loop: 'Loop',
-    start: 'Start',
-    end: 'End',
-    all: 'All',
-    reset: 'Reset',
-    disabled: 'Disabled',
-    enabled: 'Enabled',
-    advertisement: 'Ad',
-    qualityBadge: {
-      2160: '4K',
-      1440: 'HD',
-      1080: 'HD',
-      720: 'HD',
-      576: 'SD',
-      480: 'SD',
-    }
+  const i18n = { speed: 'Speed' }
   const keyboard = { global: true }
   const tooltips = { controls: false }
   const captions = { active: true }
