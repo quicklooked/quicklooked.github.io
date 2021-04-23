7c779cc58fe11cf5d90fd0b2cfea982d6d921684
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-23 23:19:43 +0700

    Update video_control.js

--- assets/js/video_control.js
+++ assets/js/video_control.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => { 
   const controls = ["play-large", "mute", "captions", "current-time", "progress", "duration", "fullscreen"]
-  const settings = ['captions']
+  const settings = ['speed']
   const i18n = { speed: 'Speed' }
   const keyboard = { global: true }
   const tooltips = { controls: false }
-  const	thumbnail = { enabled: true }
   const captions = { active: true }
   const vimeo = { referrerPolicy: 'no-referrer' }
   const fullscreen = { enabled: true , fallback: true , iosNative: false, container: '#container'}
   const speed =  { selected: 1, options: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2] }
-  const player = new Plyr('#player', { debug: true , title: '' , iconUrl: '/assets/js/sprite_video.svg' , controls , settings , i18n , thumbnail,  speed , keyboard , tooltips , captions , vimeo , fullscreen });
+  const player = new Plyr('#player', { debug: true , title: '' , iconUrl: '/assets/js/sprite_video.svg' , controls , settings , i18n,  speed , keyboard , tooltips , captions , vimeo , fullscreen });
   window.player = player;
   player.volume = 1;
   function on(selector, type, callback) {
