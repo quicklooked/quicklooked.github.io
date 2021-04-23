51ae6d787bd5c5b1010874be0ff2c043f450bbbe
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-24 03:00:27 +0700

    Update video_control.js

--- assets/js/video_control.js
+++ assets/js/video_control.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => { 
   const controls = ["play", "progress", "current-time", "mute", "volume", "captions", "settings", "pip", "airplay", "fullscreen" ]
-  const settings = ['speed', 'quality', 'caption']
+  const settings = ['captions', 'quality', 'speed']
   const i18n = { speed: 'Speed' }
   const keyboard = { global: true }
   const tooltips = { controls: false }
