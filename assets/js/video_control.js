32d2ad48999bc3536cf1f03063b6325433e5da26
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-23 23:02:47 +0700

    Update video_control.js

--- assets/js/video_control.js
+++ assets/js/video_control.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const vimeo = { referrerPolicy: 'no-referrer' }
   const fullscreen = { enabled: true , fallback: true , iosNative: false, container: '#container'}
   const speed =  { selected: 1, options: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2] }
-  const player = new Plyr('#player', { debug: true , title: '' , iconUrl: 'https://cdn.plyr.io/3.6.7/plyr.svg' , controls , settings , i18n , thumbnail,  speed , keyboard , tooltips , captions , vimeo , fullscreen });
+  const player = new Plyr('#player', { debug: true , title: '' , iconUrl: '/assets/js/sprite.svg' , controls , settings , i18n , thumbnail,  speed , keyboard , tooltips , captions , vimeo , fullscreen });
   window.player = player;
   player.volume = 1;
   function on(selector, type, callback) {
