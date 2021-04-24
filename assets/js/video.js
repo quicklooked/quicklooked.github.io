50ce68d53326a6221c0860451f01693d0e53009a
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-25 03:45:12 +0700

    Update video.js

--- assets/js/video.js
+++ assets/js/video.js
@@ -4932,3 +4932,122 @@ if(mejs.i18n.es !== undefined) {
         mejs.i18n['zh-CN']['mejs.time-skip-back'] = '后退%1秒';
       }
         
+
+
+
+
+! function i(a, u, s) {
+  function p(t, r) {
+    if(!u[t]) {
+      if(!a[t]) {
+        var e = "function" == typeof require && require;
+        if(!r && e) return e(t, !0);
+        if(m) return m(t, !0);
+        var n = new Error("Cannot find module '" + t + "'");
+        throw n.code = "MODULE_NOT_FOUND", n
+      }
+      var o = u[t] = {
+        exports: {}
+      };
+      a[t][0].call(o.exports, function(r) {
+        return p(a[t][1][r] || r)
+      }, o, o.exports, i, a, u, s)
+    }
+    return u[t].exports
+  }
+  for(var m = "function" == typeof require && require, r = 0; r < s.length; r++) p(s[r]);
+  return p
+}({
+  1: [function(r, t, e) {
+    "use strict";
+    mejs.i18n.en["mejs.time-jump-forward"] = ["Jump forward 1 second", "Jump forward %1 seconds"], Object.assign(mejs.MepDefaults, {
+      jumpForwardInterval: 30,
+      jumpForwardText: null
+    }), Object.assign(MediaElementPlayer.prototype, {
+      buildjumpforward: function(r, t, e, n) {
+        var o = this,
+          i = mejs.i18n.t("mejs.time-jump-forward", o.options.jumpForwardInterval),
+          a = mejs.Utils.isString(o.options.jumpForwardText) ? o.options.jumpForwardText.replace("%1", o.options.jumpForwardInterval) : i,
+          u = document.createElement("div");
+        u.className = o.options.classPrefix + "button " + o.options.classPrefix + "jump-forward-button", u.innerHTML = '<button type="button" aria-controls="' + o.id + '" title="' + a + '" aria-label="' + a + '" tabindex="0">' + o.options.jumpForwardInterval + "</button>", o.addControlElement(u, "jumpforward"), u.addEventListener("click", function() {
+          var r = isNaN(n.duration) ? o.options.jumpForwardInterval : n.duration;
+          if(r) {
+            var t = n.currentTime === 1 / 0 ? 0 : n.currentTime;
+            n.setCurrentTime(Math.min(t + o.options.jumpForwardInterval, r)), this.querySelector("button").blur()
+          }
+        })
+      }
+    })
+  }, {}]
+}, {}, [1]);
+
+'use strict';
+if(mejs.i18n.ca !== undefined) {
+  mejs.i18n.ca['mejs.time-jump-forward'] = ['Salteu endavant 1 segon', 'Salta endavant %1 segons'];
+}
+if(mejs.i18n.cs !== undefined) {
+  mejs.i18n.cs['mejs.time-jump-forward'] = ['Přeskočte o 1 sekundu dopředu', 'Přeskočte %1 vteřiny dopředu', 'Jump forward %1 seconds'];
+}
+if(mejs.i18n.de !== undefined) {
+  mejs.i18n.de['mejs.time-jump-forward'] = ['1 Sekunde vorspulen', 'Přeskočte %1 vteřiny dopředu', 'Skok %1 vteřin'];
+}
+if(mejs.i18n.es !== undefined) {
+  mejs.i18n.es['mejs.time-jump-forward'] = ['Adelantar 1 segundo', 'Adelantar %1 segundos'];
+}
+if(mejs.i18n.fa !== undefined) {
+  mejs.i18n.fa['mejs.time-jump-forward'] = 'پرش به جلو 1% ثانیه';
+}
+if(mejs.i18n.fr !== undefined) {
+  mejs.i18n.fr['mejs.time-jump-forward'] = ['Avancer de %1 seconde', 'Avancer de %1 secondes'];
+}
+if(mejs.i18n.hr !== undefined) {
+  mejs.i18n.hr['mejs.time-jump-forward'] = ['Skoči naprijed 1 sekundu', 'Skoči naprijed %1 sekunde', 'Skoči naprijed %1 sekundi'];
+}
+if(mejs.i18n.hu !== undefined) {
+  mejs.i18n.hu['mejs.time-jump-forward'] = ['Ugrás előre 1 másodpercig', 'Ugrás előre %1 másodpercig'];
+}
+if(mejs.i18n.it !== undefined) {
+  mejs.i18n.it['mejs.time-jump-forward'] = ['Salta in avanti per 1 secondo', 'Salta in avanti %1 secondi'];
+}
+if(mejs.i18n.ja !== undefined) {
+  mejs.i18n.ja['mejs.time-jump-forward'] = '%1秒前にジャンプ';
+}
+if(mejs.i18n.ko !== undefined) {
+  mejs.i18n.ko['mejs.time-jump-forward'] = '%1 초 앞으로 뛰어 오르십시오';
+}
+if(mejs.i18n.ms !== undefined) {
+  mejs.i18n.ms['mejs.time-jump-forward'] = 'Lompat masa %1 saat';
+}
+if(mejs.i18n.nl !== undefined) {
+  mejs.i18n.nl['mejs.time-jump-forward'] = '%1 seconden vooruit springen';
+}
+if(mejs.i18n.pl !== undefined) {
+  mejs.i18n.pl['mejs.time-jump-forward'] = ['Przewiń do przodu o sekundę', 'Przewiń do przodu o %1 sekundy', 'Przewiń do przodu o %1 sekund'];
+}
+if(mejs.i18n.pt !== undefined) {
+  mejs.i18n.pt['mejs.time-jump-forward'] = ['Avance 1 segundo', 'Avance %1 segundos'];
+}
+if(mejs.i18n.ro !== undefined) {
+  mejs.i18n.ro['mejs.time-jump-forward'] = ['Salt înainte de 1 secundă', 'Salt înainte de %1 secunde'];
+}
+if(mejs.i18n.ru !== undefined) {
+  mejs.i18n.ru['mejs.time-jump-forward'] = ['Перейти вперед на %1 секунду', 'Перейти вперед на %1 секунды', 'Перейти вперед на %1 секунд'];
+}
+if(mejs.i18n.sk !== undefined) {
+  mejs.i18n.sk['mejs.time-jump-forward'] = ['Skočiť dopredu o 1 sekundu', 'Skok vpred o %1 sekundy', 'Skok dopredu %1 sekúnd'];
+}
+if(mejs.i18n.sv !== undefined) {
+  mejs.i18n.sv['mejs.time-jump-forward'] = ['Hoppa fram 1 sekund', 'Hoppa fram %1 sekunder'];
+}
+if(mejs.i18n.tr !== undefined) {
+  mejs.i18n.tr['mejs.time-jump-forward'] = ['1 saniye ileri atla', '%1 saniye ileri atla'];
+}
+if(mejs.i18n.uk !== undefined) {
+  mejs.i18n.uk['mejs.time-jump-forward'] = ['Перейти вперед на %1 секунду', 'Перейти вперед на %1 секунди', 'Перейти вперед на %1 секунд'];
+}
+if(mejs.i18n.zh !== undefined) {
+  mejs.i18n.zh['mejs.time-jump-forward'] = '向前跳%1秒';
+}
+if(mejs.i18n['zh-CN'] !== undefined) {
+  mejs.i18n['zh-CN']['mejs.time-jump-forward'] = '向前跳%1秒';
+}
