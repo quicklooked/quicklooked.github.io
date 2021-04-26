392b80e867fed9160fee414cf195a0f7bf43681b
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-26 23:46:14 +0700

    Update user-post-sidebar.js

--- assets/js/user-post-sidebar.js
+++ assets/js/user-post-sidebar.js
@@ -21,8 +21,6 @@ function prepare() {
     document.getElementById("slide1").classList.add("vas");
     document.getElementById("slide2").classList.remove("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
     document.getElementById("slide2").classList.add("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
-    status = "over1180";
-    window.addEventListener("scroll", sync, false);
   } else {
     window.addEventListener("scroll", sync, true);
   }
@@ -39,7 +37,6 @@ function sync() {
       document.getElementById("slide2").classList.remove("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
       document.getElementById("slide2").classList.add("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
       status = "over1180";
-      window.addEventListener("scroll", prepare, false);
     }
   } else {
     if(status == "over1180") {
@@ -50,7 +47,7 @@ function sync() {
       document.getElementById("slide2").classList.remove("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
       document.getElementById("slide2").classList.add("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
       status = "under1180";
-      window.addEventListener("scroll", prepare, true);
     }
   }
+  window.addEventListener("scroll", prepare, false);
 }
