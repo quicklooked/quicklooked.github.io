3dca366639e327fadea40cf1953543662bb21e0d
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-26 23:49:03 +0700

    Update user-post-sidebar.js

--- assets/js/user-post-sidebar.js
+++ assets/js/user-post-sidebar.js
@@ -22,8 +22,14 @@ function prepare() {
     document.getElementById("slide2").classList.remove("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
     document.getElementById("slide2").classList.add("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
   } else {
-    window.addEventListener("scroll", sync, true);
+    document.getElementById("slide").classList.remove("nr", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
+    document.getElementById("slide").classList.add("em", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
+    document.getElementById("slide1").classList.remove("vas");
+    document.getElementById("slide1").classList.add("gg");
+    document.getElementById("slide2").classList.remove("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
+    document.getElementById("slide2").classList.add("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
   }
+  window.addEventListener("scroll", sync, false);
 }
 
 
