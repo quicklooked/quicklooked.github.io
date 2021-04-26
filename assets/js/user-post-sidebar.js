902f1dc320258a1e10793195028309c7f9e8f432
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-26 23:41:29 +0700

    Update user-post-sidebar.js

--- assets/js/user-post-sidebar.js
+++ assets/js/user-post-sidebar.js
@@ -14,29 +14,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 function prepare() {
-  if (x < disbar1 && x > disbar1 + disbar) {
-    if(status == "under1180") {
-      document.getElementById("slide").classList.remove("em", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
-      document.getElementById("slide").classList.add("nr", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
-      document.getElementById("slide1").classList.remove("gg");
-      document.getElementById("slide1").classList.add("vas");
-      document.getElementById("slide2").classList.remove("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
-      document.getElementById("slide2").classList.add("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
-      status = "over1180";
-      window.addEventListener("scroll", sync, false);
-  }
+  if(x < disbar1 && x > disbar1 + disbar) {
+    document.getElementById("slide").classList.remove("em", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
+    document.getElementById("slide").classList.add("nr", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
+    document.getElementById("slide1").classList.remove("gg");
+    document.getElementById("slide1").classList.add("vas");
+    document.getElementById("slide2").classList.remove("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
+    document.getElementById("slide2").classList.add("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
+    status = "over1180";
+    window.addEventListener("scroll", sync, false);
   } else {
-    if(status == "over1180") {
-      document.getElementById("slide").classList.remove("nr", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
-      document.getElementById("slide").classList.add("em", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
-      document.getElementById("slide1").classList.remove("vas");
-      document.getElementById("slide1").classList.add("gg");
-      document.getElementById("slide2").classList.remove("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
-      document.getElementById("slide2").classList.add("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
-      status = "under1180";
-      window.addEventListener("scroll", sync, false);
-    }
-  } 
+    document.getElementById("slide").classList.remove("nr", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
+    document.getElementById("slide").classList.add("em", "gg", "fj", "ml", "w", "tr", "fl", "mj", "sv");
+    document.getElementById("slide1").classList.remove("vas");
+    document.getElementById("slide1").classList.add("gg");
+    document.getElementById("slide2").classList.remove("nr", "va", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
+    document.getElementById("slide2").classList.add("em", "gg", "me", "fj", "mf", "mg", "mh", "mi", "mj", "mk");
+    status = "under1180";
+    window.addEventListener("scroll", sync, false);
+  }
 }
 
 
