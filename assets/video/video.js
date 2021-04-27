056df3524135d5064cc48717a5e284783715df88
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-28 02:01:35 +0700

    Delete video.js

--- assets/video/video.js
+++
@@ -1,7 +0,0 @@
-var player = flowplayer('#player', {
-  src: "https://l3video.lwcdn.com/hls/8fdb4e20-8ebb-4590-8844-dae39680d837/playlist_1080.m3u8",
-  title: "Thumbnail demo",
-  thumbnails: {
-    src: "https://stdlwcdn.lwcdn.com/i/8fdb4e20-8ebb-4590-8844-dae39680d837/160p.vtt"
-  }
-});
