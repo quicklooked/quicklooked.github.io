0fd4d2be726362054f8b9b640972382b39fe458e
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-04-30 18:57:06 +0700

    Update video.js

--- assets/js/video.js
+++ assets/js/video.js
@@ -59,7 +59,7 @@
       vid.play();
       video_control_play.hide();
       video_control_pause.show();
-      $(this).addClass("video-show");
+      $(video_player_controls).addClass("video-show");
     }
 
     function pause() {
