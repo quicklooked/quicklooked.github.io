598772eecf7e0d50f79484ead0842d09c40e01b8
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-05-17 21:21:46 +0700

    Create getPlaylist.js

---
+++ assets/js/video/getPlaylist.js
@@ -0,0 +1,55 @@
+function getAwesomePlaylist() {
+  const BASE_URL = 'https://storage.googleapis.com/media-session/';
+  return [{
+    sources: [{
+      src: BASE_URL + 'sintel/trailer.mp4',
+      type: 'video/mp4'
+    }],
+    name: 'How Roblox Plans to Expand Beyond Gaming',
+    duration: '52',
+    date: 'May 14',
+    description: "This week The Wall Street Journal is sitting down with industry movers and shakers for our Future of Everything Festival. Today we hear from Roblox founder and CEO David Baszucki on the videogame platform's dramatic rise over the past year, how he sees it continuing to grow, and how his company could be affected by the ongoing court battle between Epic Games and Apple. Amanda Lewellyn hosts. Christopher Zinsli is our supervising producer. Kateri Jochum is the executive producer of WSJ Podcasts. Learn more about your ad choices. Visit megaphone.fm/adchoices",
+    poster: 'https://i.scdn.co/image/c274a8abb1d57d18f0c4f1d45d73aadd4c9f18dd',
+    thumbnail: [{
+      srcset: 'https://i.scdn.co/image/c274a8abb1d57d18f0c4f1d45d73aadd4c9f18dd',
+      type: 'image/png',
+      media: '(width: 112px; height: 112px;)'
+    }, {
+      src: 'https://i.scdn.co/image/c274a8abb1d57d18f0c4f1d45d73aadd4c9f18dd'
+    }],
+  }, {
+    sources: [{
+      src: BASE_URL + 'big-buck-bunny/trailer.mov',
+      type: 'video/mp4'
+    }],
+    name: 'How Roblox Plans to Expand Beyond Gaming',
+    duration: '32',
+    date: 'May 15',
+    description: "This week The Wall Street Journal is sitting down with industry movers and shakers for our Future of Everything Festival. Today we hear from Roblox founder and CEO David Baszucki on the videogame platform's dramatic rise over the past year, how he sees it continuing to grow, and how his company could be affected by the ongoing court battle between Epic Games and Apple. Amanda Lewellyn hosts. Christopher Zinsli is our supervising producer. Kateri Jochum is the executive producer of WSJ Podcasts. Learn more about your ad choices. Visit megaphone.fm/adchoices",
+    poster: 'https://i.scdn.co/image/c274a8abb1d57d18f0c4f1d45d73aadd4c9f18dd',
+    thumbnail: [{
+      srcset: 'https://i.scdn.co/image/c274a8abb1d57d18f0c4f1d45d73aadd4c9f18dd',
+      type: 'image/png',
+      media: '(width: 112px; height: 112px;)'
+    }, {
+      src: 'https://i.scdn.co/image/c274a8abb1d57d18f0c4f1d45d73aadd4c9f18dd'
+    }],
+  }, {
+    sources: [{
+      src: BASE_URL + 'elephants-dream/teaser.mp4',
+      type: 'video/mp4'
+    }],
+    name: 'How Roblox Plans to Expand Beyond Gaming',
+    duration: '75',
+    date: 'May 16',
+    description: "This week The Wall Street Journal is sitting down with industry movers and shakers for our Future of Everything Festival. Today we hear from Roblox founder and CEO David Baszucki on the videogame platform's dramatic rise over the past year, how he sees it continuing to grow, and how his company could be affected by the ongoing court battle between Epic Games and Apple. Amanda Lewellyn hosts. Christopher Zinsli is our supervising producer. Kateri Jochum is the executive producer of WSJ Podcasts. Learn more about your ad choices. Visit megaphone.fm/adchoices",
+    poster: 'https://i.scdn.co/image/c274a8abb1d57d18f0c4f1d45d73aadd4c9f18dd',
+    thumbnail: [{
+      srcset: 'https://i.scdn.co/image/c274a8abb1d57d18f0c4f1d45d73aadd4c9f18dd',
+      type: 'image/png',
+      media: '(width: 112px; height: 112px;)'
+    }, {
+      src: 'https://i.scdn.co/image/c274a8abb1d57d18f0c4f1d45d73aadd4c9f18dd'
+    }],
+  }];
+}
