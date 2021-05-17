0e11cd8a0b85be651bf79616e83ae0af27b364c2
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-05-18 00:27:14 +0700

    Update get-playlist.js

--- assets/js/video/get-playlist.js
+++ assets/js/video/get-playlist.js
@@ -12,11 +12,11 @@ function getAwesomePlaylist() {
     description: "This week The Wall Street Journal is sitting down with industry movers and shakers for our Future of Everything Festival. Today we hear from Roblox founder and CEO David Baszucki on the videogame platform's dramatic rise over the past year, how he sees it continuing to grow, and how his company could be affected by the ongoing court battle between Epic Games and Apple. Amanda Lewellyn hosts. Christopher Zinsli is our supervising producer. Kateri Jochum is the executive producer of WSJ Podcasts. Learn more about your ad choices. Visit megaphone.fm/adchoices",
     poster: 'https://m.wsj.net/video/20200710/20200710chromeyfc5/20200710chromeyfc5_960x540.jpg',
     thumbnail: [{
-      srcset: 'https://i.scdn.co/image/c274a8abb1d57d18f0c4f1d45d73aadd4c9f18dd',
+      srcset: 'https://m.wsj.net/video/20200710/20200710chromeyfc5/20200710chromeyfc5_960x540.jpg',
       type: 'image/png',
       media: '(width: 112px; height: 112px;)'
     }, {
-      src: 'https://i.scdn.co/image/c274a8abb1d57d18f0c4f1d45d73aadd4c9f18dd'
+      src: 'https://m.wsj.net/video/20200710/20200710chromeyfc5/20200710chromeyfc5_960x540.jpg'
     }],
   }, {
     sources: [{
