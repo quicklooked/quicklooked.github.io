34828c0a12557b0eb04094aef7a29624c1802157
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-05-18 21:39:55 +0700

    Update get-playlist.js

--- assets/js/video/get-playlist.js
+++ assets/js/video/get-playlist.js
@@ -3,7 +3,7 @@ function getAwesomePlaylist() {
   return [{
     sources: [{
       src: 'https://m.wsj.net/video/20200710/20200710chromeyfc5/hls/manifest-hd-wifi.m3u8',
-      type: 'video/m3u8'
+      type: 'application/x-mpegURL'
     }],
     textTracks:[ { "kind":"subtitles", "label":"English", "src":"https://m.wsj.net/video/20200710/20200710chromeyfc5/20200710chromeyfc5.en_US.vtt", "srclang":"en" } ],
     name: 'Quit Chrome. Safari and Edge Are Just Better Browsers for You and Your Computer.',
@@ -21,7 +21,7 @@ function getAwesomePlaylist() {
   }, {
     sources: [{
       src: 'https://m.wsj.net/video/20210504/040421yellenecon/hls/manifest-hd-wifi.m3u8',
-      type: 'video/m3u8'
+      type: 'application/x-mpegURL'
     }],
     textTracks:[ { "kind":"subtitles", "label":"English", "src":"https://m.wsj.net/video/20210504/040421yellenecon/040421yellenecon.en_US.vtt", "srclang":"en" } ],
     name: 'Treasury Secretary Yellen Doesn\'t Anticipate Inflation to Be a Problem',
@@ -39,7 +39,7 @@ function getAwesomePlaylist() {
   }, {
     sources: [{
       src: 'https://m.wsj.net/video/20210516/pipelinecyberattack051621/hls/manifest-hd-wifi.m3u8',
-      type: 'video/m3u8'
+      type: 'application/x-mpegURL'
     }],
     textTracks:[ { "kind":"subtitles", "label":"English", "src":"https://m.wsj.net/video/20210516/pipelinecyberattack051621/pipelinecyberattack051621.en_US.vtt", "srclang":"en" } ],
     name: 'How Vulnerable Is U.S. Energy Infrastructure to Future Cyberattacks?',
