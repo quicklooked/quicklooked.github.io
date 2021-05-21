7007900548306bcc9300f9aae551ec6748da2a73
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-05-21 22:07:31 +0700

    Update get-playlist.js

--- assets/js/video/get-playlist.js
+++ assets/js/video/get-playlist.js
@@ -9,6 +9,8 @@ function getAwesomePlaylist() {
     name: 'The Costs and Benefits of a $15 Federal Minimum Wage',
     duration: '503',
     date: 'May 14',
+    authors: 'The Economist',
+    authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
     description: "Biden has identified raising the minimum wage as a key goal of his administration, but economists and lawmakers disagree on the potential impact. WSJ asked two economists and a minimum-wage worker what the costs and benefits of a $15 minimum wage might be. Photo: Bill Clark/Congressional Quarterly/Zuma Press",
     poster: 'https://m.wsj.net/video/20210301/030221minwagesplash/030221minwagesplash_960x540.jpg',
     thumbnail: [{
@@ -27,6 +29,8 @@ function getAwesomePlaylist() {
     name: 'Tesla vs. NIO: Battle for the World’s Largest EV Market',
     duration: '388',
     date: 'May 17',
+    authors: 'The Economist',
+    authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
     description: "Gone are the long waits at charging stations: Chinese electric-vehicle startup NIO is pioneering battery-swap systems, challenging Tesla and other rival car makers. Here’s how NIO and Tesla are racing for the world’s largest EV market in China. Photo illustration: Sharon Shi",
     poster: 'https://m.wsj.net/video/20210104/010521nio2/010521nio2_960x540.jpg',
     thumbnail: [{
@@ -45,6 +49,8 @@ function getAwesomePlaylist() {
     name: 'How Vulnerable Is U.S. Energy Infrastructure to Future Cyberattacks?',
     duration: '352',
     date: 'May 19',
+    authors: 'The Economist',
+    authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
     description: "A cyberattack on the U.S.’s largest fuel pipeline on May 7 forced a shutdown that triggered a spike in gas prices and shortages in parts of the Southeast. WSJ explains just how vulnerable the nation’s critical energy infrastructure is to attack. Photo illustration: Liz Ornitz/WSJ",
     poster: 'https://m.wsj.net/video/20210516/pipelinecyberattack051621/pipelinecyberattack051621_960x540.jpg',
     thumbnail: [{
