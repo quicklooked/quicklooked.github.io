b17f3eb1032bc1eafd480a38d9bf77699280dae8
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-05-22 15:24:16 +0700

    Update get-playlist-the-economist.js

--- assets/js/video/get-playlist-the-economist.js
+++ assets/js/video/get-playlist-the-economist.js
@@ -42,5 +42,48 @@ function getPlaylistTheEconomist() {
     description: "A cyberattack on the U.S.’s largest fuel pipeline on May 7 forced a shutdown that triggered a spike in gas prices and shortages in parts of the Southeast. WSJ explains just how vulnerable the nation’s critical energy infrastructure is to attack. Photo illustration: Liz Ornitz/WSJ",
     poster: 'https://m.wsj.net/video/20210516/pipelinecyberattack051621/pipelinecyberattack051621_960x540.jpg',
     thumbnail: [{ src: 'https://m.wsj.net/video/20210516/pipelinecyberattack051621/pipelinecyberattack051621_960x540.jpg' }],
+  }, {
+    sources: [{
+      src: 'https://m.wsj.net/video/20210505/05042021streetwisesalim/hls/manifest-hd-wifi.m3u8',
+      type: 'application/x-mpegURL'
+    }],
+    textTracks:[ { "kind":"subtitles", "label":"English", "src":"https://m.wsj.net/video/20210505/05042021streetwisesalim/05042021streetwisesalim.en_US.vtt", "srclang":"en" } ],
+    name: '50 Years\' Worth of Indexing',
+    duration: '175',
+    date: 'May 22',
+    authors: 'The Economist',
+    authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
+    description: "Salim Ramji, global head of iShares and index investments for BlackRock, discusses innovations in indexing and how investors can balance active and passive management.",
+    poster: 'https://m.wsj.net/video/20210505/05042021streetwisesalim/05042021streetwisesalim_960x540.jpg',
+    thumbnail: [{ src: 'https://m.wsj.net/video/20210505/05042021streetwisesalim/05042021streetwisesalim_960x540.jpg' }],
+         
+  }, {
+    sources: [{
+      src: 'https://m.wsj.net/video/20210426/04262021mbep97/hls/manifest-hd-wifi.m3u8',
+      type: 'application/x-mpegURL'
+    }],
+    textTracks:[ { "kind":"subtitles", "label":"English", "src":"https://m.wsj.net/video/20210426/04262021mbep97/04262021mbep97.en_US.vtt", "srclang":"en" } ],
+    name: 'Low Rates, Rising Yields & Your Money',
+    duration: '563',
+    date: 'May 22',
+    authors: 'The Economist',
+    authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
+    description: "Ahead of this week\'s Fed meeting, Dana Peterson, chief economist with The Conference Board, unpacks the central bank\'s role amid the ongoing economic recovery. Plus, Lee Ferridge of State Street Global Markets discusses his firm\'s bull case for gold and big tech.",
+    poster: 'https://m.wsj.net/video/20210426/04262021mb97/04262021mb97_1280x720.jpg',
+    thumbnail: [{ src: 'https://m.wsj.net/video/20210426/04262021mb97/04262021mb97_1280x720.jpg' }],
+  }, {
+    sources: [{
+      src: 'https://m.wsj.net/video/20210407/04072021swoil/hls/manifest-hd-wifi.m3u8',
+      type: 'application/x-mpegURL'
+    }],
+    textTracks:[ { "kind":"subtitles", "label":"English", "src":"https://m.wsj.net/video/20210407/04072021swoil/04072021swoil.en_US.vtt", "srclang":"en" } ],
+    name: 'What\'s next for oil prices? Here\'s what to know',
+    duration: '245',
+    date: 'May 22',
+    authors: 'The Economist',
+    authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
+    description: "It\'s been nearly a year since the unprecedented collapse in oil prices and investors are wondering what\'s next. Here are the main price drivers to watch out for.",
+    poster: 'https://m.wsj.net/video/20210407/04062021swoil/04062021swoil_1280x720.jpg',
+    thumbnail: [{ src: 'https://m.wsj.net/video/20210407/04062021swoil/04062021swoil_1280x720.jpg' }],
   }];
 }
