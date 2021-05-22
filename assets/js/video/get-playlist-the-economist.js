014948519eff260800ec7bcb73340fba4913ac07
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-05-22 17:18:28 +0700

    Update get-playlist-the-economist.js

--- assets/js/video/get-playlist-the-economist.js
+++ assets/js/video/get-playlist-the-economist.js
@@ -5,7 +5,12 @@ function getPlaylistTheEconomist() {
       src: 'https://m.wsj.net/video/20210301/030221minwage/hls/manifest-hd-wifi.m3u8',
       type: 'application/x-mpegURL'
     }],
-    textTracks:[ { "kind":"subtitles", "label":"English", "src":"https://m.wsj.net/video/20210301/030221minwage/030221minwage.en_US.vtt", "srclang":"en" } ],
+    textTracks: [{
+      "kind": "subtitles",
+      "label": "English",
+      "src": "https://m.wsj.net/video/20210301/030221minwage/030221minwage.en_US.vtt",
+      "srclang": "en"
+    }],
     name: 'The Costs and Benefits of a $15 Federal Minimum Wage',
     duration: '503',
     date: 'May 14',
@@ -13,13 +18,20 @@ function getPlaylistTheEconomist() {
     authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
     description: "Biden has identified raising the minimum wage as a key goal of his administration, but economists and lawmakers disagree on the potential impact. WSJ asked two economists and a minimum-wage worker what the costs and benefits of a $15 minimum wage might be. Photo: Bill Clark/Congressional Quarterly/Zuma Press",
     poster: 'https://m.wsj.net/video/20210301/030221minwagesplash/030221minwagesplash_960x540.jpg',
-    thumbnail: [{ src: 'https://m.wsj.net/video/20210301/030221minwagesplash/030221minwagesplash_960x540.jpg' }],
+    thumbnail: [{
+      src: 'https://m.wsj.net/video/20210301/030221minwagesplash/030221minwagesplash_960x540.jpg'
+    }],
   }, {
     sources: [{
       src: 'https://m.wsj.net/video/20210104/010521nio2/hls/manifest-hd-wifi.m3u8',
       type: 'application/x-mpegURL'
     }],
-    textTracks:[ { "kind":"subtitles", "label":"English", "src":"https://m.wsj.net/video/20210104/010521nio2/010521nio2.en_US.vtt", "srclang":"en" } ],
+    textTracks: [{
+      "kind": "subtitles",
+      "label": "English",
+      "src": "https://m.wsj.net/video/20210104/010521nio2/010521nio2.en_US.vtt",
+      "srclang": "en"
+    }],
     name: 'Tesla vs. NIO: Battle for the World’s Largest EV Market',
     duration: '388',
     date: 'May 17',
@@ -27,63 +39,239 @@ function getPlaylistTheEconomist() {
     authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
     description: "Gone are the long waits at charging stations: Chinese electric-vehicle startup NIO is pioneering battery-swap systems, challenging Tesla and other rival car makers. Here’s how NIO and Tesla are racing for the world’s largest EV market in China. Photo illustration: Sharon Shi",
     poster: 'https://m.wsj.net/video/20210104/010521nio2/010521nio2_960x540.jpg',
-    thumbnail: [{ src: 'https://m.wsj.net/video/20210104/010521nio2/010521nio2_960x540.jpg' }],
+    thumbnail: [{
+      src: 'https://m.wsj.net/video/20210104/010521nio2/010521nio2_960x540.jpg'
+    }],
   }, {
     sources: [{
       src: 'https://m.wsj.net/video/20210516/pipelinecyberattack051621/hls/manifest-hd-wifi.m3u8',
       type: 'application/x-mpegURL'
     }],
-    textTracks:[ { "kind":"subtitles", "label":"English", "src":"https://m.wsj.net/video/20210516/pipelinecyberattack051621/pipelinecyberattack051621.en_US.vtt", "srclang":"en" } ],
+    textTracks: [{
+      "kind": "subtitles",
+      "label": "English",
+      "src": "https://m.wsj.net/video/20210516/pipelinecyberattack051621/pipelinecyberattack051621.en_US.vtt",
+      "srclang": "en"
+    }],
     name: 'How Vulnerable Is U.S. Energy Infrastructure to Future Cyberattacks?',
     duration: '352',
-    date: 'May 19',
+    date: 'May 18',
     authors: 'The Economist',
     authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
     description: "A cyberattack on the U.S.’s largest fuel pipeline on May 7 forced a shutdown that triggered a spike in gas prices and shortages in parts of the Southeast. WSJ explains just how vulnerable the nation’s critical energy infrastructure is to attack. Photo illustration: Liz Ornitz/WSJ",
     poster: 'https://m.wsj.net/video/20210516/pipelinecyberattack051621/pipelinecyberattack051621_960x540.jpg',
-    thumbnail: [{ src: 'https://m.wsj.net/video/20210516/pipelinecyberattack051621/pipelinecyberattack051621_960x540.jpg' }],
+    thumbnail: [{
+      src: 'https://m.wsj.net/video/20210516/pipelinecyberattack051621/pipelinecyberattack051621_960x540.jpg'
+    }],
   }, {
     sources: [{
       src: 'https://m.wsj.net/video/20210505/05042021streetwisesalim/hls/manifest-hd-wifi.m3u8',
       type: 'application/x-mpegURL'
     }],
-    textTracks:[ { "kind":"subtitles", "label":"English", "src":"https://m.wsj.net/video/20210505/05042021streetwisesalim/05042021streetwisesalim.en_US.vtt", "srclang":"en" } ],
+    textTracks: [{
+      "kind": "subtitles",
+      "label": "English",
+      "src": "https://m.wsj.net/video/20210505/05042021streetwisesalim/05042021streetwisesalim.en_US.vtt",
+      "srclang": "en"
+    }],
     name: '50 Years\' Worth of Indexing',
     duration: '175',
-    date: 'May 22',
+    date: 'May 19',
     authors: 'The Economist',
     authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
     description: "Salim Ramji, global head of iShares and index investments for BlackRock, discusses innovations in indexing and how investors can balance active and passive management.",
     poster: 'https://m.wsj.net/video/20210505/05042021streetwisesalim/05042021streetwisesalim_960x540.jpg',
-    thumbnail: [{ src: 'https://m.wsj.net/video/20210505/05042021streetwisesalim/05042021streetwisesalim_960x540.jpg' }],
-         
+    thumbnail: [{
+      src: 'https://m.wsj.net/video/20210505/05042021streetwisesalim/05042021streetwisesalim_960x540.jpg'
+    }],
   }, {
     sources: [{
       src: 'https://m.wsj.net/video/20210426/04262021mbep97/hls/manifest-hd-wifi.m3u8',
       type: 'application/x-mpegURL'
     }],
-    textTracks:[ { "kind":"subtitles", "label":"English", "src":"https://m.wsj.net/video/20210426/04262021mbep97/04262021mbep97.en_US.vtt", "srclang":"en" } ],
+    textTracks: [{
+      "kind": "subtitles",
+      "label": "English",
+      "src": "https://m.wsj.net/video/20210426/04262021mbep97/04262021mbep97.en_US.vtt",
+      "srclang": "en"
+    }],
     name: 'Low Rates, Rising Yields & Your Money',
     duration: '563',
-    date: 'May 22',
+    date: 'May 20',
     authors: 'The Economist',
     authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
     description: "Ahead of this week\'s Fed meeting, Dana Peterson, chief economist with The Conference Board, unpacks the central bank\'s role amid the ongoing economic recovery. Plus, Lee Ferridge of State Street Global Markets discusses his firm\'s bull case for gold and big tech.",
     poster: 'https://m.wsj.net/video/20210426/04262021mb97/04262021mb97_1280x720.jpg',
-    thumbnail: [{ src: 'https://m.wsj.net/video/20210426/04262021mb97/04262021mb97_1280x720.jpg' }],
+    thumbnail: [{
+      src: 'https://m.wsj.net/video/20210426/04262021mb97/04262021mb97_1280x720.jpg'
+    }],
   }, {
     sources: [{
       src: 'https://m.wsj.net/video/20210407/04072021swoil/hls/manifest-hd-wifi.m3u8',
       type: 'application/x-mpegURL'
     }],
-    textTracks:[ { "kind":"subtitles", "label":"English", "src":"https://m.wsj.net/video/20210407/04072021swoil/04072021swoil.en_US.vtt", "srclang":"en" } ],
+    textTracks: [{
+      "kind": "subtitles",
+      "label": "English",
+      "src": "https://m.wsj.net/video/20210407/04072021swoil/04072021swoil.en_US.vtt",
+      "srclang": "en"
+    }],
     name: 'What\'s next for oil prices? Here\'s what to know',
     duration: '245',
-    date: 'May 22',
+    date: 'May 21',
     authors: 'The Economist',
     authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
     description: "It\'s been nearly a year since the unprecedented collapse in oil prices and investors are wondering what\'s next. Here are the main price drivers to watch out for.",
     poster: 'https://m.wsj.net/video/20210407/04062021swoil/04062021swoil_1280x720.jpg',
-    thumbnail: [{ src: 'https://m.wsj.net/video/20210407/04062021swoil/04062021swoil_1280x720.jpg' }],
+    thumbnail: [{
+      src: 'https://m.wsj.net/video/20210407/04062021swoil/04062021swoil_1280x720.jpg'
+    }],
+  }, {
+    sources: [{
+      src: 'https://m.wsj.net/video/20210322/03222021mb92rev/hls/manifest-hd-wifi.m3u8',
+      type: 'application/x-mpegURL'
+    }],
+    textTracks: [{
+      "kind": "subtitles",
+      "label": "English",
+      "src": "https://m.wsj.net/video/20210322/03222021mb92rev/03222021mb92rev.en_US.vtt",
+      "srclang": "en"
+    }],
+    name: 'Wall Street\'s Next Frontier',
+    duration: '576',
+    date: 'May 22',
+    authors: 'The Economist',
+    authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
+    description: "As investors pursue new opportunities in space, Dylan Taylor, CEO of Voyager Space Holdings, weighs in. Plus, Katie Nixon, CIO of Northern Trust Wealth Management, unpacks her bull case for U.S. stocks.",
+    poster: 'https://m.wsj.net/video/20210322/03222021mb92/03222021mb92_1280x720.jpg',
+    thumbnail: [{
+      src: 'https://m.wsj.net/video/20210322/03222021mb92/03222021mb92_1280x720.jpg'
+    }],
+  }, {
+    sources: [{
+      src: 'https://m.wsj.net/video/20210312/03122021barronsbitcoin/hls/manifest-hd-wifi.m3u8',
+      type: 'application/x-mpegURL'
+    }],
+    textTracks: [{
+      "kind": "subtitles",
+      "label": "English",
+      "src": "https://m.wsj.net/video/20210312/03122021barronsbitcoin/03122021barronsbitcoin.en_US.vtt",
+      "srclang": "en"
+    }],
+    name: 'The Future of Bitcoin',
+    duration: '152',
+    date: 'May 22',
+    authors: 'The Economist',
+    authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
+    description: "Institutional investors are playing a major role in Bitcoin\'s recent uptick. Here\'s why, and what it means for the future.",
+    poster: 'https://m.wsj.net/video/20210312/03122021barronsbitcoin/03122021barronsbitcoin_960x540.jpg',
+    thumbnail: [{
+      src: 'https://m.wsj.net/video/20210312/03122021barronsbitcoin/03122021barronsbitcoin_960x540.jpg'
+    }],
+  }, {
+    sources: [{
+      src: 'https://m.wsj.net/video/20210224/02242021swpc/hls/manifest-hd-wifi.m3u8',
+      type: 'application/x-mpegURL'
+    }],
+    textTracks: [{
+      "kind": "subtitles",
+      "label": "English",
+      "src": "https://m.wsj.net/video/20210224/02242021swpc/02242021swpc.en_US.vtt",
+      "srclang": "en"
+    }],
+    name: 'Desktops are making a comeback during the pandemic',
+    duration: '218',
+    date: 'May 22',
+    authors: 'The Economist',
+    authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
+    description: "The remote-work boom has amplified growth in the PC market. Here are 3 ways the pandemic is changing the future of PC design.",
+    poster: 'https://m.wsj.net/video/20210224/02242021swpc/02242021swpc_1280x720.jpg',
+    thumbnail: [{
+      src: 'https://m.wsj.net/video/20210224/02242021swpc/02242021swpc_1280x720.jpg'
+    }],
+  }, {
+    sources: [{
+      src: 'https://m.wsj.net/video/20210212/02122021swhospitality/hls/manifest-hd-wifi.m3u8',
+      type: 'application/x-mpegURL'
+    }],
+    textTracks: [{
+      "kind": "subtitles",
+      "label": "English",
+      "src": "https://m.wsj.net/video/20210212/02122021swhospitality/02122021swhospitality.en_US.vtt",
+      "srclang": "en"
+    }],
+    name: '3 major changes in the hotel industry due to COVID-19',
+    duration: '235',
+    date: 'May 22',
+    authors: 'The Economist',
+    authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
+    description: "Hotel owners lost nearly $46 billion in revenue last year. Here\'s how the industry is shifting its business model and pivoting towards tech to recover.",
+    poster: 'https://m.wsj.net/video/20210212/02122021swhospitality1/02122021swhospitality1_1280x720.jpg',
+    thumbnail: [{
+      src: 'https://m.wsj.net/video/20210212/02122021swhospitality1/02122021swhospitality1_1280x720.jpg'
+    }],
+  }, {
+    sources: [{
+      src: 'https://m.wsj.net/video/20210211/02112021exvday/hls/manifest-hd-wifi.m3u8',
+      type: 'application/x-mpegURL'
+    }],
+    textTracks: [{
+      "kind": "subtitles",
+      "label": "English",
+      "src": "https://m.wsj.net/video/20210211/02112021exvday/02112021exvday.en_US.vtt",
+      "srclang": "en"
+    }],
+    name: '3 money mistakes couples make and how to avoid them',
+    duration: '171',
+    date: 'May 22',
+    authors: 'The Economist',
+    authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
+    description: "Talking to your partner about your finances may not be romantic, but it is crucial to a healthy relationship. Here\'s how to spot and navigate common pitfalls.",
+    poster: 'https://m.wsj.net/video/20210211/02112021exvday/02112021exvday_1280x720.jpg',
+    thumbnail: [{
+      src: 'https://m.wsj.net/video/20210211/02112021exvday/02112021exvday_1280x720.jpg'
+    }],
+  }, {
+    sources: [{
+      src: 'https://m.wsj.net/video/20210209/02092021swspace/hls/manifest-hd-wifi.m3u8',
+      type: 'application/x-mpegURL'
+    }],
+    textTracks: [{
+      "kind": "subtitles",
+      "label": "English",
+      "src": "https://m.wsj.net/video/20210209/02092021swspace/02092021swspace.en_US.vtt",
+      "srclang": "en"
+    }],
+    name: 'How to invest in the space economy in 2021',
+    duration: '208',
+    date: 'May 22',
+    authors: 'The Economist',
+    authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
+    description: "Scientific and technological advancements have made the space industry more accessible to everyday investors. Here\'s what to know.",
+    poster: 'https://m.wsj.net/video/20210209/02092021swspace/02092021swspace_1280x720.jpg',
+    thumbnail: [{
+      src: 'https://m.wsj.net/video/20210209/02092021swspace/02092021swspace_1280x720.jpg'
+    }],
+  }, {
+    sources: [{
+      src: 'https://m.wsj.net/video/20210208/02082021exshortsell/hls/manifest-hd-wifi.m3u8',
+      type: 'application/x-mpegURL'
+    }],
+    textTracks: [{
+      "kind": "subtitles",
+      "label": "English",
+      "src": "https://m.wsj.net/video/20210208/02082021exshortsell/02082021exshortsell.en_US.vtt",
+      "srclang": "en"
+    }],
+    name: 'What is short selling and should you do it?',
+    duration: '159',
+    date: 'May 22',
+    authors: 'The Economist',
+    authorsImg: 'https://www.economist.com/engassets/ico/favicon.f1ea908894.ico',
+    description: "The GameStop saga brought short selling into the spotlight for many new investors. Here\'s what it is and why some investors use it.",
+    poster: 'https://m.wsj.net/video/20210208/02082021exshortsell/02082021exshortsell_1280x720.jpg',
+    thumbnail: [{
+      src: 'https://m.wsj.net/video/20210208/02082021exshortsell/02082021exshortsell_1280x720.jpg'
+    }],
   }];
 }
