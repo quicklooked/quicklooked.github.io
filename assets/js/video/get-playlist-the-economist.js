0407e6e1172686fe3afccefde1628780f7126c3c
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-05-25 23:39:05 +0700

    Update get-playlist-the-economist.js

--- assets/js/video/get-playlist-the-economist.js
+++ assets/js/video/get-playlist-the-economist.js
@@ -5,7 +5,7 @@ function getPlaylistTheEconomist() {
       src: 'https://m.wsj.net/video/20210301/030221minwage/hls/manifest-hd-wifi.m3u8',
       type: 'application/x-mpegURL'
     }],
-    textTracks: [{
+    audioTracks: [{
       "kind": "descriptions",
       "label": "English Audio",
       "src": "https://www.ca11y.com/videojs-speak-descriptions-track/descriptions.en.vtt",
