5629604ad78da4da2c2e7b45c57c2cacb3bb3174
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-06-21 00:36:21 +0700

    Update jwplayer.defaults.js

--- assets/css/uservideos/jwplayer.defaults.js
+++ assets/css/uservideos/jwplayer.defaults.js
@@ -1,4 +1,4 @@
-var vast_tag = "http://pubads.g.doubleclick.net/gampad/ads?sz=640x360&iu=/64986556/tvchosun_VAST&ciu_szs&impl=s&gdfp_req=1&env=vp&output=xml_vast2&unviewed_position_start=1&correlator=" + new Date().getTime();
+var vast_tag = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator==" + new Date().getTime();
 
 var jwDefaults = {
   "key": "GytdaYuqfyvirEusXu6WqDo4Flbquswk8SceF+iP+chw318UDdxUxovv3nFnvgsa",
