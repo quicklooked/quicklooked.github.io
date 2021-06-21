2660e3d661070448e1987a3a77152c5b611aea60
TheQuicklook <70672236+quicklooked@users.noreply.github.com>
2021-06-21 13:48:12 +0700

    Update jwplayer.defaults.js

--- assets/css/uservideos/jwplayer.defaults.js
+++ assets/css/uservideos/jwplayer.defaults.js
@@ -28,7 +28,8 @@ var jwDefaults = {
   "advertising": {
     "client": "vast",
     //"skipoffset": 5,
-    "admessage": "Video will play after ad",
+    "admessage": 'quicklook.vn<span class="ytp-ad-button-icon"><svg fill="#fff" height="100%" version="1.1" viewBox="0 0 48 48" width="100%"><path d="M0 0h48v48H0z" fill="none"></path><path d="M38 38H10V10h14V6H10c-2.21 0-4 1.79-4 4v28c0 2.21 1.79 4 4 4h28c2.21 0 4-1.79 4-4V24h-4v14zM28 6v4h7.17L15.51 29.66l2.83 2.83L38 12.83V20h4V6H28z"></path></svg></span>',
+    //"admessage": "Video will play after ad",
     //"skipmessage": 'Video will play after ad',
     //"skiptext": "Skip ad",
     //"skipmessage": 'Skip this ad in XXs',
